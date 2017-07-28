b5b8346826ce3eef1b246810030d36e162b7e703
sukesh-ust <sukesh.sugunan@ust-global.com>
2017-07-28 20:11:48 +0530

    Modified create.sh script

--- installscripts/terraform-unix-noinstances-jazz/scripts/create.sh
+++ installscripts/terraform-unix-noinstances-jazz/scripts/create.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+aws_secret_access_key=`tr -d '\n' < ~/.aws/credentials | sed -e 's/\[/\n[/g'|grep default|sed -e 's/\[default\]//g' -e 's/aws_secret_access_key/\naws_secret_access_key/g' -e  's/ //g' |grep aws_secret_access_key| cut -d'=' -f2`
+aws_access_key=`tr -d '\n' < ~/.aws/credentials | sed -e 's/\[/\n[/g'|grep default|sed -e 's/\[default\]//g' -e 's/aws_secret_access_key/\naws_secret_access_key/g' -e  's/ //g'| grep "aws_access_key" |cut -d'=' -f2`
+sed -i "s/AWS_ACCESS_KEY=.*.$/AWS_ACCESS_KEY=$aws_access_key/g" ../cookbooks/jenkins/files/credentials/aws.sh
+sed -i "s|AWS_SECRET_KEY=.*.$|AWS_SECRET_KEY=$aws_secret_access_key|g" ../cookbooks/jenkins/files/credentials/aws.sh
 date
 terraform apply
 date
-
 
