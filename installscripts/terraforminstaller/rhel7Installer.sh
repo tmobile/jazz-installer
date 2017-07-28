723958e225ac9299f31b9b37b6b7c13e2f89daa4
ustharin <harin.jose@ust-global.com>
2017-07-28 10:48:50 -0400

    Update rhel7Installer.sh
    Former-commit-id: 459ee1bdd784120e63e23dc5e3f194f2b3cfb49a

--- installscripts/terraforminstaller/rhel7Installer.sh
+++ installscripts/terraforminstaller/rhel7Installer.sh
@@ -19,7 +19,7 @@ sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 #---------------------------------------
 sudo curl -L https://releases.hashicorp.com/terraform/0.9.11/terraform_0.9.11_linux_amd64.zip?_ga=2.191030627.850923432.1499789921-755991382.1496973261 -o /tmp/terraform.zip
 sudo curl -L https://releases.hashicorp.com/packer/1.0.2/packer_1.0.2_linux_amd64.zip?_ga=2.211418261.1015376711.1499791279-168406014.1496924698 -o /tmp/packer.zip
-sudo curl -L https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -o /usr/local/bin/jq
+sudo curl -L https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -o /usr/local/bin/jq; sudo chmod 755 /usr/local/bin/jq
 cd /usr/bin; sudo unzip /tmp/terraform.zip; sudo unzip /tmp/packer.zip
 cd /home/ec2-user; sudo curl -L https://bobswift.atlassian.net/wiki/download/attachments/16285777/atlassian-cli-6.7.1-distribution.zip -o /tmp/atlassian-cli-6.7.1-distribution.zip; sudo unzip /tmp/atlassian-cli-6.7.1-distribution.zip
 #----------npm
