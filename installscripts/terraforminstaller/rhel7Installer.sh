e48e0d4dbd939063d5db461228fbd141ff09a4cd
ustharin <harin.jose@ust-global.com>
2017-07-29 13:39:38 -0400

    Update rhel7Installer.sh
    Former-commit-id: 02af47fb946903a00f74e0e733e4b22cc54cca82

--- installscripts/terraforminstaller/rhel7Installer.sh
+++ installscripts/terraforminstaller/rhel7Installer.sh
@@ -39,6 +39,7 @@ pwd
 npm
 ng version
 git clone -b patch-5 https://ustharin:Tmobiledemo1@github.com/tmobile/jazz-installer.git
+chmod -R +x ./jazz-installer/installscripts/*
 chmod 400 ./jazz-installer/installscripts/sshkeys/*
 cd ./jazz-installer/installscripts/wizard
 aws configure
