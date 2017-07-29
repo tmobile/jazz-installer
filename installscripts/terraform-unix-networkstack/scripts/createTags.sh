e355cfe8f307b563c15b51a063a79c066deadab9
ustharin <harin.jose@ust-global.com>
2017-07-29 14:05:19 -0400

    Update createTags.sh
    fixed sed command
    
    Former-commit-id: 4a70ff71040e7376ccca4a6bb93cd92515d5a176

--- installscripts/terraform-unix-networkstack/scripts/createTags.sh
+++ installscripts/terraform-unix-networkstack/scripts/createTags.sh
@@ -5,8 +5,8 @@ ENVIRONMENT=$3
 EXEMPT=$4
 OWNER=$5
 TAGSFILE=$6
-sed -i "s|variable \"envPrefix\" { type = \"string\"  default.*.$|variable \"envPrefix\" { type = \"string\"  default = \"$ENVPREFIX\" }|g" $TAGSFILE
-sed -i "s|variable \"tagsApplication\" { type = \"string\"  default.*.$|variable \"tagsApplication\" { type = \"string\"  default = \"$APPLICATION\" }|g" $TAGSFILE
-sed -i "s|variable \"tagsEnvironment\" {type = \"string\" default.*.$|variable \"tagsEnvironment\" {type = \"string\" default = \"$ENVIRONMENT\" }|g" $TAGSFILE
-sed -i "s|variable \"tagsExempt\" { type = \"string\" default.*.$|variable \"tagsExempt\" { type = \"string\" default = \"$EXEMPT\" }|g" $TAGSFILE
-sed -i "s|variable \"tagsOwner\" { type = \"string\" default.*.$|variable \"tagsOwner\" { type = \"string\" default = \"$OWNER\" }|g" $TAGSFILE
+sed -i "s|variable \"envPrefix\".*.$|variable \"envPrefix\" \{ type = \"string\"  default = \"$ENVPREFIX\" \}|g" $TAGSFILE
+sed -i "s|variable \"tagsApplication\".*.$|variable \"tagsApplication\" \{ type = \"string\"  default = \"$APPLICATION\" \}|g" $TAGSFILE
+sed -i "s|variable \"tagsEnvironment\".*.$|variable \"tagsEnvironment\" \{type = \"string\" default = \"$ENVIRONMENT\" \}|g" $TAGSFILE
+sed -i "s|variable \"tagsExempt\".*.$|variable \"tagsExempt\" \{ type = \"string\" default = \"$EXEMPT\" \}|g" $TAGSFILE
+sed -i "s|variable \"tagsOwner\".*.$|variable \"tagsOwner\" \{ type = \"string\" default = \"$OWNER\" \}|g" $TAGSFILE
