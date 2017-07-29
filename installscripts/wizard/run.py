ba2a98207f45d935531d890855ee3ce21a2943a4
ustharin <harin.jose@ust-global.com>
2017-07-29 13:32:31 -0400

    Update run.py
    Former-commit-id: b6a62380b7386172f115c9cb334118d1d33dec60

--- installscripts/wizard/run.py
+++ installscripts/wizard/run.py
@@ -9,7 +9,7 @@ tagEnvPrefix = raw_input("Please provide the tag Name to Prefix your Stack(Eg:- 
 tagApplication="JAZZ"
 tagEnvironment="Development"
 tagExempt="09/01/2017"
-tagOwner="sukesh.sugunan@ust-global.com"
+tagOwner="sukesh.sugunan"
 fullstack = raw_input("Do you need full stack including network(Y/N): ") 
 
 if fullstack == "y" or  fullstack == "Y" : # no inputs fomr the client. Create network stack and Jenkins and bitbucket servers
