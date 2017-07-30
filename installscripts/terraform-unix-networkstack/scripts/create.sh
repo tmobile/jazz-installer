5b6324bf8e67eef063b6531bc2e12b70aad3f946
ustharin <harin.jose@ust-global.com>
2017-07-30 10:17:15 -0400

    Update create.sh
    Former-commit-id: 2cfc35b277a016f12705758cafab4a96e7deb227

--- installscripts/terraform-unix-networkstack/scripts/create.sh
+++ installscripts/terraform-unix-networkstack/scripts/create.sh
@@ -2,3 +2,11 @@
 date
 terraform apply
 date
+echo " ======================================================="
+echo " The following Stack has been created in AWS"
+echo " ________________________________________________"
+terraform state list
+echo " ======================================================="
+echo " Once checkout is done Please execute nohup ./destroy.sh & in the following directory . This will cleanup the entire Stack"
+pwd
+echo " ======================================================="
