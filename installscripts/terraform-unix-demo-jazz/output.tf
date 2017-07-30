65015881c1749fda4dbe51132a759f27e00a8759
ustharin <harin.jose@ust-global.com>
2017-07-30 09:58:40 -0400

    Update output.tf
    added cloudfront domain name and added ports to jenkins and bitbucket elbs
    
    Former-commit-id: fad440840a159d623fdf5b075b48afde771e7d69

--- installscripts/terraform-unix-demo-jazz/output.tf
+++ installscripts/terraform-unix-demo-jazz/output.tf
@@ -1,13 +1,13 @@
 resource "null_resource" "outputVariables" {
   depends_on = ["aws_elb_attachment.bitbucket","aws_elb_attachment.jenkins" ]
   provisioner "local-exec" {
-    command = "echo bitbucketelb= ${aws_elb.bitbucketelb.dns_name} > settings.txt"
+    command = "echo bitbucketelb = http://${aws_elb.bitbucketelb.dns_name}:7990 > settings.txt"
   }
   provisioner "local-exec" {
     command = "echo bitbucket publicip = ${aws_instance.bitbucketserver.public_ip} >> settings.txt"
   }
   provisioner "local-exec" {
-    command = "echo jenkinselb= ${aws_elb.jenkinselb.dns_name} >> settings.txt"
+    command = "echo jenkinselb = http://${aws_elb.jenkinselb.dns_name}:8080 >> settings.txt"
   }
   provisioner "local-exec" {
     command = "echo jenkins-publicip = ${aws_instance.jenkinsserver.public_ip} >> settings.txt"
@@ -17,6 +17,9 @@ resource "null_resource" "outputVariables" {
   }
   provisioner "local-exec" {
     command = "echo jenkins-securitygroup = ${aws_security_group.jenkins.id} >> settings.txt"
+  }
+  provisioner "local-exec" {
+    command = "echo cloudfront url = http://${aws_cloudfront_distribution.jazz.domain_name}/index.html >> settings.txt"
   }
 }
 
