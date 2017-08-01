14cf23324b3bfbcc3dfc76616e8048e6cf0806b1
ustharin <harin.jose@ust-global.com>
2017-08-01 16:41:08 -0400

    Update configureS3Names.sh

--- installscripts/terraform-unix-demo-jazz/scripts/configureS3Names.sh
+++ installscripts/terraform-unix-demo-jazz/scripts/configureS3Names.sh
@@ -18,5 +18,5 @@ sed -i "s/jazz_bucket_dev.*.$/jazz_bucket_dev=$jazz_bucket_dev/g" $jenkinspropsf
 sed -i "s/jazz_bucket_stg.*.$/jazz_bucket_stg=$jazz_bucket_stg/g" $jenkinspropsfile
 sed -i "s/jazz_bucket_prod.*.$/jazz_bucket_prod=$jazz_bucket_prod/g" $jenkinspropsfile
 sed -i "s/jazz_bucket_cloudfrontlogs.*.$/jazz_bucket_cloudfrontlogs=$jazz_bucket_cloudfrontlogs/g" $jenkinspropsfile
-sed -i "s/jazz_bucket_dev.*.$/jazz_bucket_dev=$jazz_bucket_dev/g" $jenkinspropsfile
+sed -i "s/jazz_bucket_web.*.$/jazz_bucket_web=$jazz_bucket_web/g" $jenkinspropsfile
 
