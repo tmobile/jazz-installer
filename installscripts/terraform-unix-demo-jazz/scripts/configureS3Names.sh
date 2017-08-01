4595947f9b30caf246bd52d9911ec27e5a8a4637
sukesh-ust <sukesh.sugunan@ust-global.com>
2017-08-02 02:36:34 +0530

    Corrected typo

--- installscripts/terraform-unix-demo-jazz/scripts/configureS3Names.sh
+++ installscripts/terraform-unix-demo-jazz/scripts/configureS3Names.sh
@@ -3,15 +3,9 @@
 jazz_bucket_dev=$1
 jazz_bucket_stg=$2
 jazz_bucket_prod=$3
-jazz-bucket_cloudfrontlogs=$4
+jazz_bucket_cloudfrontlogs=$4
 jazz_bucket_web=$5
 jenkinspropsfile=$6
-
-echo $1
-echo $2
-echo $3
-echo $4
-echo $5
 
 # new changes for randomizing s3 bucket names
 sed -i "s/jazz_bucket_dev.*.$/jazz_bucket_dev=$jazz_bucket_dev/g" $jenkinspropsfile
