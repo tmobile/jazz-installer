8d9793a0b7898f99ba0544796b54450322ab3f95
sukesh-ust <sukesh.sugunan@ust-global.com>
2017-08-02 01:20:12 +0530

    Corrected sed comman din configureS3Names.sh file

--- installscripts/terraform-unix-demo-jazz/scripts/configureS3Names.sh
+++ installscripts/terraform-unix-demo-jazz/scripts/configureS3Names.sh
@@ -6,9 +6,17 @@ jazz_bucket_prod=$3
 jazz-bucket_cloudfrontlogs=$4
 jazz_bucket_web=$5
 jenkinspropsfile=$6
+
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+
 # new changes for randomizing s3 bucket names
-sed -i "s/jazz_bucket_dev.*.$/jazz_bucket_dev=$jazz_bucket_dev=/g" $jenkinspropsfile
-sed -i "s/jazz_bucket_stg.*.$/jazz_bucket_stg=$jazz_bucket_stg=/g" $jenkinspropsfile
-sed -i "s/jazz_bucket_prod.*.$/jazz_bucket_prod=$jazz_bucket_prod=/g" $jenkinspropsfile
-sed -i "s/jazz_bucket_cloudfrontlogs.*.$/jazz_bucket_cloudfrontlogs=$jazz_bucket_cloudfrontlogs=/g" $jenkinspropsfile
-sed -i "s/jazz_bucket_web.*.$/jazz_bucket_web=$jazz_bucket_web=/g" $jenkinspropsfile
+sed -i "s/jazz_bucket_dev.*.$/jazz_bucket_dev=$jazz_bucket_dev/g" $jenkinspropsfile
+sed -i "s/jazz_bucket_stg.*.$/jazz_bucket_stg=$jazz_bucket_stg/g" $jenkinspropsfile
+sed -i "s/jazz_bucket_prod.*.$/jazz_bucket_prod=$jazz_bucket_prod/g" $jenkinspropsfile
+sed -i "s/jazz_bucket_cloudfrontlogs.*.$/jazz_bucket_cloudfrontlogs=$jazz_bucket_cloudfrontlogs/g" $jenkinspropsfile
+sed -i "s/jazz_bucket_dev.*.$/jazz_bucket_dev=$jazz_bucket_dev/g" $jenkinspropsfile
+
