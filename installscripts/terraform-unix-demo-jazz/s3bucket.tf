7f815fec939a3ff67099c7d32c2c9418df2522cc
ustharin <harin.jose@ust-global.com>
2017-08-01 14:28:25 -0400

    Update s3bucket.tf
    added new provisioner for s3bucket name randomizing
    
    Former-commit-id: b66cbfaab1e737df2459ccff43f08199a6e27949

--- installscripts/terraform-unix-demo-jazz/s3bucket.tf
+++ installscripts/terraform-unix-demo-jazz/s3bucket.tf
@@ -236,3 +236,8 @@ resource "aws_iam_role_policy_attachment" "cloudwatchlogaccess" {
     role       = "${aws_iam_role.lambda_role.name}"
     policy_arn = "${aws_iam_policy.basic_execution_policy.arn}"
 }
+resource "null_resource" "configureS3Names" {
+ provisioner "local-exec" {
+    command = "${var.configureS3Names_cmd} ${aws_s3_bucket.oab-apis-deployment-dev.bucket.id} ${aws_s3_bucket.oab-apis-deployment-stg.bucket.id} ${aws_s3_bucket.oab-apis-deployment-prod.bucket.id} ${aws_s3_bucket.cloudfrontlogs.bucket.id} ${aws_s3_bucket.jazz-web.bucket.id} ${var.jenkinspropsfile} "
+  }
+}
