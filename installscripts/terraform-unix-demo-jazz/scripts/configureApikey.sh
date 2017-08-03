44ed521ed8183da113840909f436ad5704252f2a
sukesh-ust <sukesh.sugunan@ust-global.com>
2017-08-03 21:19:25 +0530

    Corrected the file path
    Former-commit-id: f7da499f89b9a552aa4a518f2020624b42aa9919

--- installscripts/terraform-unix-demo-jazz/scripts/configureApikey.sh
+++ installscripts/terraform-unix-demo-jazz/scripts/configureApikey.sh
@@ -24,4 +24,4 @@ sed -i "s/{inst_region}/$region/g" ./jazz-ui/src/config/config.json
 
 # Changing jazz-web config.json
 sed -i "s/{API_GATEWAY_KEY_DEV\}/$API_ID_DEV/g" ./jazz-core/jazz-web/config/config.json
-sed -i "s/{inst_region}/$region/g" ./jazz-core/jazz-web/config.json
+sed -i "s/{inst_region}/$region/g" ./jazz-core/jazz-web/config/config.json
