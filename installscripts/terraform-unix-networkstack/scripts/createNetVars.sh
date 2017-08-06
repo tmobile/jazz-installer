b70d7d95c4cc761dbbac9f32181f1470563e8d6c
ustharin <harin.jose@ust-global.com>
2017-08-05 23:32:21 -0400

    Update createNetVars.sh
    no need for VPC and CIDR. both are retrieved from subnet

--- installscripts/terraform-unix-networkstack/scripts/createNetVars.sh
+++ installscripts/terraform-unix-networkstack/scripts/createNetVars.sh
@@ -3,6 +3,4 @@ VPC=$1
 SUBNET=$2
 CIDRBLOCKS=$3
 NETVARSFILE=$4
-sed -i "s|variable \"vpc\" { type = \"string\"  default.*.$|variable \"vpc\" { type = \"string\"  default = \"$VPC\" }|g" $NETVARSFILE
 sed -i "s|variable \"subnet\" {type = \"string\" default.*.$|variable \"subnet\" {type = \"string\" default = \"$SUBNET\" }|g" $NETVARSFILE
-sed -i "s|variable \"cidrblocks\" { type = \"string\" default.*.$|variable \"cidrblocks\" { type = \"string\" default = \"$CIDRBLOCKS\" }|g" $NETVARSFILE
