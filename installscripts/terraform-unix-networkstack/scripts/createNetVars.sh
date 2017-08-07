c736ab964ec49ba46871625d6b979be8ef7fd600
ustharin <harin.jose@ust-global.com>
2017-08-07 13:40:06 -0400

    Update createNetVars.sh
    moved the vpc and cidr
    
    Former-commit-id: 5aa666f4619492a54719a86e3dbfa9960d156532

--- installscripts/terraform-unix-networkstack/scripts/createNetVars.sh
+++ installscripts/terraform-unix-networkstack/scripts/createNetVars.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-VPC=$1
-SUBNET=$2
-CIDRBLOCKS=$3
-NETVARSFILE=$4
+SUBNET=$1
+NETVARSFILE=$2
 sed -i "s|variable \"subnet\" {type = \"string\" default.*.$|variable \"subnet\" {type = \"string\" default = \"$SUBNET\" }|g" $NETVARSFILE
