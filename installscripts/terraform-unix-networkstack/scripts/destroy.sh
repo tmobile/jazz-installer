ca3a96c535370c1da4b09ffb2765288756f0ad58
sukeshss-ust <sukesh.sugunan@ust-global.com>
2017-08-24 14:18:45 +0530

    Changing destroy script to print progress.
    Former-commit-id: f89cf09d7ed2ee3bfa4e49e4835be99588e16333

--- installscripts/terraform-unix-networkstack/scripts/destroy.sh
+++ installscripts/terraform-unix-networkstack/scripts/destroy.sh
@@ -1,5 +1,28 @@
-
 #!/bin/bash
 date
+
+currentDir=`pwd`
+
+echo " ======================================================="
+echo " The following Stack has been marked for deletion in AWS"
+echo " ________________________________________________"
+cd ../terraform-unix-demo-jazz
+terraform state list
+
+cd ../terraform-unix-networkstack
+terraform state list
+echo " ======================================================="
+
+cd ../terraform-unix-demo-jazz
+
+terraform destroy --force &&
+
+cd ../terraform-unix-networkstack
+
 terraform destroy --force
-date
+
+
+echo " Destroying of stack Initiated!!! "
+echo " Execute  tail -f nohup.out ' in below directory to see the stack deletion progress"
+echo $currentDir
+echo " ======================================================="
