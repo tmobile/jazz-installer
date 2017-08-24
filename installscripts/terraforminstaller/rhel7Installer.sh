de17273ee9d87f043c88894fe500e76744a15a28
sukeshss-ust <sukesh.sugunan@ust-global.com>
2017-08-24 11:37:05 +0530

    Add files via upload
    Former-commit-id: f697873a8811ff46789064432019edb453e4c453

--- installscripts/terraforminstaller/rhel7Installer.sh
+++ installscripts/terraforminstaller/rhel7Installer.sh
@@ -9,18 +9,21 @@ sudo rpm -ivh ./jdk-8u112-linux-x64.rpm
 #-----------aws
 sudo yum install -y unzip
 sudo curl -L https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -o /tmp/awscli-bundle.zip
-cd /tmp; sudo unzip awscli-bundle.zip
+cd /tmp; sudo rm -rf awscli-bundle; sudo unzip awscli-bundle.zip
+sudo rm -rf /usr/local/aws
+sudo rm -f /usr/local/bin/aws
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 #---------------------------------------
 sudo curl -L https://releases.hashicorp.com/terraform/0.9.11/terraform_0.9.11_linux_amd64.zip?_ga=2.191030627.850923432.1499789921-755991382.1496973261 -o /tmp/terraform.zip
 sudo curl -L https://releases.hashicorp.com/packer/1.0.2/packer_1.0.2_linux_amd64.zip?_ga=2.211418261.1015376711.1499791279-168406014.1496924698 -o /tmp/packer.zip
 sudo curl -L https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -o /usr/local/bin/jq; sudo chmod 755 /usr/local/bin/jq
-cd /usr/bin; sudo unzip /tmp/terraform.zip; sudo unzip /tmp/packer.zip
-cd /home/ec2-user; sudo curl -L https://bobswift.atlassian.net/wiki/download/attachments/16285777/atlassian-cli-6.7.1-distribution.zip -o /tmp/atlassian-cli-6.7.1-distribution.zip; sudo unzip /tmp/atlassian-cli-6.7.1-distribution.zip
+cd /usr/bin; sudo unzip -o /tmp/terraform.zip; sudo unzip -o /tmp/packer.zip
+cd /home/ec2-user; sudo curl -L https://bobswift.atlassian.net/wiki/download/attachments/16285777/atlassian-cli-6.7.1-distribution.zip -o /tmp/atlassian-cli-6.7.1-distribution.zip; sudo unzip -o /tmp/atlassian-cli-6.7.1-distribution.zip
 
+rm -rf jazz-installer
 git clone https://ustharin:Tmobiledemo1@github.com/tmobile/jazz-installer.git
 chmod -R +x ./jazz-installer/installscripts/*
-chmod 400 ./jazz-installer/installscripts/sshkeys/*
+chmod -R 400 ./jazz-installer/installscripts/sshkeys/*
 cd /home/ec2-user/jazz-installer/installscripts/wizard
 aws configure
