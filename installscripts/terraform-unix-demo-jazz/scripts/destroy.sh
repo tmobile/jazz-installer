d587c98aff44b2c2ffaf49a53d4579139f18f1f0
sukeshss-ust <sukesh.sugunan@ust-global.com>
2017-08-25 13:49:46 +0530

    changing destroy script

--- installscripts/terraform-unix-demo-jazz/scripts/destroy.sh
+++ installscripts/terraform-unix-demo-jazz/scripts/destroy.sh
@@ -15,8 +15,6 @@ echo " Execute  'tail -f stack_deletion.out'  in below directory to see the stac
 echo $currentDir
 echo " ======================================================="
 
-nohup terraform destroy --force >>../wizard/stack_deletion.out &&
+nohup terraform destroy --force >> ../wizard/stack_deletion.out &&
 
-
-
-
+date
