f2d44eacacd73e1c50e0afdaf0500dd625183f5b
sukeshss-ust <sukesh.sugunan@ust-global.com>
2017-08-25 12:15:00 +0530

    changing message in destroy script.
    Former-commit-id: cc64cc725588e1261bbcc8d774a765c128f557f9

--- installscripts/terraform-unix-demo-jazz/scripts/destroy.sh
+++ installscripts/terraform-unix-demo-jazz/scripts/destroy.sh
@@ -10,12 +10,13 @@ cd ../terraform-unix-demo-jazz
 terraform state list
 
 echo " ======================================================="
-
-nohup terraform destroy --force >>../wizard/stack_deletion.out &&
-
-
 echo " Destroying of stack Initiated!!! "
 echo " Execute  'tail -f stack_deletion.out'  in below directory to see the stack deletion progress"
 echo $currentDir
 echo " ======================================================="
+
+nohup terraform destroy --force >>../wizard/stack_deletion.out &&
+
+
+
 
