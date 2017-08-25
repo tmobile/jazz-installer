1c9aff1b2a4ab2049897c66ace30b6028912ef90
sukeshss-ust <sukesh.sugunan@ust-global.com>
2017-08-25 12:04:09 +0530

    changing message in destroy script.
    Former-commit-id: 4c15a0df8460f69c799633c828fb34fe3a3c762d

--- installscripts/terraform-unix-networkstack/scripts/destroy.sh
+++ installscripts/terraform-unix-networkstack/scripts/destroy.sh
@@ -13,16 +13,16 @@ cd ../terraform-unix-networkstack
 terraform state list
 echo " ======================================================="
 
+echo " Destroying of stack Initiated!!! "
+echo " Execute  'tail -f stack_deletion.out' and 'tail -f network_stack_deletion.out' in below directory to see the stack deletion progress"
+echo $currentDir
+echo " ======================================================="
+
 cd ../terraform-unix-demo-jazz
 
 nohup terraform destroy --force >>../wizard/stack_deletion.out &&
 
 cd ../terraform-unix-networkstack
 
-nohup terraform destroy -var "cidrblocks=$cidr" --force >>../wizard/network_stack_deletion.out &
+nohup terraform destroy -var "cidrblocks=$cidr" --force >>../wizard/network_stack_deletion.out &&
 
-
-echo " Destroying of stack Initiated!!! "
-echo " Execute  'tail -f stack_deletion.out' and 'tail -f network_stack_deletion.out' in below directory to see the stack deletion progress"
-echo $currentDir
-echo " ======================================================="
