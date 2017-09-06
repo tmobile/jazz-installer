ce5e5dd45c3c997ee4fc0f31cd8207130eb23ee3
unknown <U22958@E155LTRV.ustr.com>
2017-09-06 11:32:53 +0530

    Adding new services
    Former-commit-id: 6e61c659eca9cd5e02eb4af9c6a0adc2f8b76d09

--- installscripts/terraform/modifyCodebase.cmd
+++ installscripts/terraform/modifyCodebase.cmd
@@ -23,3 +23,28 @@ sed -i 's/securityGroupIds.*.$/securityGroupIds: %securityGroupIds%/g' ./jazz-co
 sed -i 's/subnetIds.*.$/subnetIds: %subnetIds%/g' ./jazz-core/api-template-python/deployment-env.yml
 sed -i 's=iamRoleARN.*.$=iamRoleARN: %iamRoleARN%=g' ./jazz-core/api-template-python/deployment-env.yml
 sed -i 's/region.*.$/region: %region%/g' ./jazz-core/api-template-python/deployment-env.yml
+
+sed -i 's/securityGroupIds.*.$/securityGroupIds: %securityGroupIds%/g' ./jazz-core/platform-services-handler/deployment-env.yml
+sed -i 's/subnetIds.*.$/subnetIds: %subnetIds%/g' ./jazz-core/platform-services-handler/deployment-env.yml
+sed -i 's=iamRoleARN.*.$=iamRoleARN: %iamRoleARN%=g' ./jazz-core/platform-services-handler/deployment-env.yml
+sed -i 's/region.*.$/region: %region%/g' ./jazz-core/platform-services-handler/deployment-env.yml
+
+sed -i 's/securityGroupIds.*.$/securityGroupIds: %securityGroupIds%/g' ./jazz-core/platform_events/deployment-env.yml
+sed -i 's/subnetIds.*.$/subnetIds: %subnetIds%/g' ./jazz-core/platform_events/deployment-env.yml
+sed -i 's=iamRoleARN.*.$=iamRoleARN: %iamRoleARN%=g' ./jazz-core/platform_events/deployment-env.yml
+sed -i 's/region.*.$/region: %region%/g' ./jazz-core/platform_events/deployment-env.yml
+
+sed -i 's/securityGroupIds.*.$/securityGroupIds: %securityGroupIds%/g' ./jazz-core/lambda-template-java/deployment-env.yml
+sed -i 's/subnetIds.*.$/subnetIds: %subnetIds%/g' ./jazz-core/lambda-template-java/deployment-env.yml
+sed -i 's=iamRoleARN.*.$=iamRoleARN: %iamRoleARN%=g' ./jazz-core/lambda-template-java/deployment-env.yml
+sed -i 's/region.*.$/region: %region%/g' ./jazz-core/lambda-template-java/deployment-env.yml
+
+sed -i 's/securityGroupIds.*.$/securityGroupIds: %securityGroupIds%/g' ./jazz-core/lambda-template-nodejs/deployment-env.yml
+sed -i 's/subnetIds.*.$/subnetIds: %subnetIds%/g' ./jazz-core/lambda-template-nodejs/deployment-env.yml
+sed -i 's=iamRoleARN.*.$=iamRoleARN: %iamRoleARN%=g' ./jazz-core/lambda-template-nodejs/deployment-env.yml
+sed -i 's/region.*.$/region: %region%/g' ./jazz-core/lambda-template-nodejs/deployment-env.yml
+
+sed -i 's/securityGroupIds.*.$/securityGroupIds: %securityGroupIds%/g' ./jazz-core/lambda-template-python/deployment-env.yml
+sed -i 's/subnetIds.*.$/subnetIds: %subnetIds%/g' ./jazz-core/lambda-template-python/deployment-env.yml
+sed -i 's=iamRoleARN.*.$=iamRoleARN: %iamRoleARN%=g' ./jazz-core/lambda-template-python/deployment-env.yml
+sed -i 's/region.*.$/region: %region%/g' ./jazz-core/lambda-template-python/deployment-env.yml
