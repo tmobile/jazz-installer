552e031b6ac4c6c2e0dcc94a412edf852ffefd21
unknown <U22958@E155LTRV.ustr.com>
2017-09-06 18:26:03 +0530

    Changing the node version
    Former-commit-id: a7e193bb9e0de6452aeec7a12dbeae3982f35bb7

--- installscripts/terraform-unix-demo-jazz/variables.tf
+++ installscripts/terraform-unix-demo-jazz/variables.tf
@@ -32,7 +32,7 @@ variable "bitbucketserver_ami" {
 variable "jenkinsserver_ami" {
   type = "string"
 	// ami in us-east-1
-  default = "ami-7a121a01"
+  default = "ami-5a293a21"
 	// amis copied to us-west-1
  // default = "ami-ae745dce"
 }
