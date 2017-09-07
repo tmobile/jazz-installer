dc59974959118ce76617f7adf3856a6921dc1142
unknown <U22958@E155LTRV.ustr.com>
2017-09-07 11:57:09 +0530

    Adding new dynamodb tables
    Former-commit-id: 0da136c7fcd8dafc1661b06eaa8e5636f57d63f3

--- installscripts/terraform-unix-demo-jazz/commands.tf
+++ installscripts/terraform-unix-demo-jazz/commands.tf
@@ -55,4 +55,8 @@ variable "deployS3Webapp_cmd" {
   type = "string"
   default = "./scripts/deployS3Webapp.sh"
 }
+variable "dynamodb_cmd" {
+  type = "string"
+  default = "./scripts/dynamodb.sh"
+}
 
