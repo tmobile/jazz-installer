a98462dead1c588004c8ea97b384e71ee355754b
jenkins1 <harin.jose@ust-global.com>
2017-09-12 13:15:43 +0000

    Escaping the characters in property file
    Former-commit-id: bbc4850bd1bb493253eef887dd557dd56225f031

--- installscripts/terraform-unix-demo-jazz/scripts/modifyPropertyFile.sh
+++ installscripts/terraform-unix-demo-jazz/scripts/modifyPropertyFile.sh
@@ -3,6 +3,7 @@
 property_key=$1
 property_value=$2
 property_file=$3
+property_value=$(sed 's/[]\/$*.^|[]/\\&/g' <<< $property_value)
 
 sed -i "s/$property_key.*.$/$property_key=$property_value/g" $property_file
 
