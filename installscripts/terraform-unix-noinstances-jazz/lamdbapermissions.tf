fe74da9d08567aeff9ce95ebf6f00799a3228568
Ajeesh <Ajeesh.Ameen@ust-global.com>
2017-09-14 20:06:09 +0530

    deleting lambda permission
    Former-commit-id: cf979dd8edf4f59ccb419e9ec80e948fbd5e8024

--- installscripts/terraform-unix-noinstances-jazz/lamdbapermissions.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_lambda_permission" "allow_cloudwatch" {
-  statement_id   = "${lookup(var.lambdaCloudWatchProps, "statement_id")}"
-  action         = "${lookup(var.lambdaCloudWatchProps, "action")}"
-  function_name  = "${lookup(var.lambdaCloudWatchProps, "function_name")}"
-  principal      = "${lookup(var.lambdaCloudWatchProps, "principal")}"
-}
-
