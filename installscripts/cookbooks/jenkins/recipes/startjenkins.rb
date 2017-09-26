cf12dea6da3a565b0389368b0ce87160e0f74f72
jenkins1 <harin.jose@ust-global.com>
2017-09-26 09:10:19 +0000

    changes to move git branch name to props file
    Former-commit-id: 04583de61b5a0597d7321b24334a83782ebdcc58

--- installscripts/cookbooks/jenkins/recipes/startjenkins.rb
+++ installscripts/cookbooks/jenkins/recipes/startjenkins.rb
@@ -60,7 +60,7 @@ end
 #end
 
 execute 'downloadgitproj' do
-  command "/usr/local/git/bin/git clone -b development https://ustharin:Tmobiledemo1@github.com/tmobile/jazz-core.git"
+  command "/usr/local/git/bin/git clone -b JAZZBRANCH https://ustharin:Tmobiledemo1@github.com/tmobile/jazz-core.git"
   cwd '/home/ec2-user'
 end
 # downloading and running mvn assembly will be don on installer box. This will be uploaded to jenkins master using scp
