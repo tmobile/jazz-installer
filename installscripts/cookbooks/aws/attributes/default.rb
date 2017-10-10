d056f6583da7ad4c9f3e54877839d1306065673a
ajeeshameen <Nithin.Akkarapatty1@t-mobile.com>
2017-10-10 18:35:17 +0530

    removing outout

--- installscripts/cookbooks/aws/attributes/default.rb
+++ installscripts/cookbooks/aws/attributes/default.rb
@@ -1,4 +1,4 @@
-default['aws']['USER_ID']    = 'serverless-admin'
-default['aws']['ACCESS_KEY_ID']    = 'AKIAIAZLUOXNHOC5HQ5A'
-default['aws']['SECRET_ACCESS_KEY']    = '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'
+default['aws']['USER_ID']    = ''
+default['aws']['ACCESS_KEY_ID']    = ''
+default['aws']['SECRET_ACCESS_KEY']    = ''
 
