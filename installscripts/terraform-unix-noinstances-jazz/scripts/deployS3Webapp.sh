f3279827b1244b7a4f92573c3b4cf435aa64bfcc
ajeeshameen <ajeesh.ameen@ust-global.com>
2017-10-11 09:41:20 -0400

    Updating files s3bucket.tf, sets3acl.sh and deployS3Webapp.sh
    Former-commit-id: 547f12dcdaa96a4d4653075ca863c73e8ae96e20

--- installscripts/terraform-unix-noinstances-jazz/scripts/deployS3Webapp.sh
+++ installscripts/terraform-unix-noinstances-jazz/scripts/deployS3Webapp.sh
@@ -1,12 +1,13 @@
 BUCKET_NAME=$1
 REGION=$2
+CANONICAL_ID=$3
 cd ./jazz-core/jazz-web
 aws s3 cp . s3://$BUCKET_NAME  --recursive --region $REGION
 
 IFS=$(echo -en "\n\b")
 for key in $( find . \( -not -type d \) -print | sed 's/^.\///g' ); do
     echo item: $key
-         aws s3api put-object-acl --bucket $BUCKET_NAME --key $key --grant-full-control id=78d7d8174c655d51683784593fe4e6f74a7ed3fae3127d2beca2ad39e4fdc79a,uri=http://acs.amazonaws.com/groups/s3/LogDelivery,uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers --grant-read uri=http://acs.amazonaws.com/groups/global/AllUsers   --grant-read-acp uri=http://acs.amazonaws.com/groups/global/AllUsers
+         aws s3api put-object-acl --bucket $BUCKET_NAME --key $key --grant-full-control id=$CANONICAL_ID,uri=http://acs.amazonaws.com/groups/s3/LogDelivery,uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers --grant-read uri=http://acs.amazonaws.com/groups/global/AllUsers   --grant-read-acp uri=http://acs.amazonaws.com/groups/global/AllUsers
 done
 cd -
 
