547f12dcdaa96a4d4653075ca863c73e8ae96e20
ajeeshameen <ajeesh.ameen@ust-global.com>
2017-10-11 09:41:20 -0400

    Updating files s3bucket.tf, sets3acl.sh and deployS3Webapp.sh

--- installscripts/terraform-unix-demo-jazz/scripts/sets3acl.sh
+++ installscripts/terraform-unix-demo-jazz/scripts/sets3acl.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 BUCKET_NAME=$1
-aws s3api put-bucket-acl --bucket $BUCKET_NAME --grant-full-control id=78d7d8174c655d51683784593fe4e6f74a7ed3fae3127d2beca2ad39e4fdc79a,uri=http://acs.amazonaws.com/groups/s3/LogDelivery,uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers --grant-read uri=http://acs.amazonaws.com/groups/global/AllUsers   --grant-read-acp uri=http://acs.amazonaws.com/groups/global/AllUsers
+CANONICAL_ID=$2
+aws s3api put-bucket-acl --bucket $BUCKET_NAME --grant-full-control id=$CANONICAL_ID,uri=http://acs.amazonaws.com/groups/s3/LogDelivery,uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers --grant-read uri=http://acs.amazonaws.com/groups/global/AllUsers   --grant-read-acp uri=http://acs.amazonaws.com/groups/global/AllUsers
