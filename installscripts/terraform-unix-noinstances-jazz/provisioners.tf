2fb0011697d72ac5015ea2594a6a7c30f5557ac2
jenkins1 <harin.jose@ust-global.com>
2017-10-28 07:38:34 +0000

    changes to handle standalone jenkins
    Former-commit-id: c00d353213626babd1a9d5b0947f4febe39fe413

--- installscripts/terraform-unix-noinstances-jazz/provisioners.tf
+++ installscripts/terraform-unix-noinstances-jazz/provisioners.tf
@@ -14,41 +14,72 @@ resource "null_resource" "configureExistingJenkinsServer" {
   provisioner "local-exec" {
     command = "${var.configurebitbucketelb_cmd} ${lookup(var.bitbucketservermap, "bitbucket_elb")}  ${var.chefconfigDir}/bitbucketelbconfig.json ${var.jenkinsattribsfile} ${var.jenkinspropsfile} ${var.bitbucketclient_cmd} ${var.envPrefix}"
   }
-  provisioner "file" {
-          source      = "${var.cookbooksDir}/jenkins/recipes"
-          destination = "~/cookbooks/jenkins/"
+   provisioner "file" {
+          source      = "${var.cookbooksDir}/"
+          destination = "~/cookbooks"
   }
   provisioner "file" {
-          source      = "${var.cookbooksDir}/jenkins/files/default"
-          destination = "~/cookbooks/jenkins/files/"
-  }
-  provisioner "file" {
-          source      = "${var.cookbooksDir}/jenkins/files/jobs"
-          destination = "~/cookbooks/jenkins/files"
-  }
-  provisioner "file" {
-          source      = "${var.cookbooksDir}/jenkins/files/node"
-          destination = "~/cookbooks/jenkins/files"
-  }
-  provisioner "file" {
-          source      = "${var.cookbooksDir}/jenkins/files/scriptapproval"
-          destination = "~/cookbooks/jenkins/files"
-  }
-  provisioner "file" {
-          source      = "${var.cookbooksDir}/jenkins/files/credentials"
-          destination = "~/cookbooks/jenkins/files"
-  }
-  provisioner "file" {
-          source      = "${var.cookbooksDir}/jenkins/attributes"
-          destination = "~/cookbooks/jenkins/"
-  }
-  provisioner "file" {
-          source      = "jazz-core/aws-apigateway-importer"
-          destination = "/tmp"
-  }
+          source      = "${var.chefconfigDir}/"
+          destination = "~/chefconfig"
+  } 
+
  provisioner "remote-exec" {
     inline = [
-          "sudo chef-client --local-mode -c ~/chefconfig/client.rb --override-runlist jenkins::configureblankjenkins"
+          "sudo sh ~/cookbooks/installChef.sh",
+                  "sudo cd /var/lib/jenkins/",
+                  "sudo cat ~/cookbooks/jenkins/files/plugins/plugins0* >plugins.tar",
+                  "sudo tar -xf plugins.tar -C /var/lib/jenkins/",
+                  "sudo curl -O https://bootstrap.pypa.io/get-pip.py&& sudo python get-pip.py",
+                  "sudo chmod -R o+w /usr/lib/python2.7/site-packages/ /usr/bin/",
+                  "sudo cp ~/.bash_profile /var/lib/jenkins; sudo cp ~/.bashrc /var/lib/jenkins",
+                  "sudo chef-client --local-mode -c ~/chefconfig/client.rb -j ~/chefconfig/node-jenkinsserver-packages.json"
+    ]
+  }
+     provisioner "file" {
+          source      = "${var.cookbooksDir}/"
+          destination = "~/cookbooks"
+  }
+ 
+  provisioner "file" {
+          source      = "${var.cookbooksDir}/jenkins/recipes/"
+          destination = "~/cookbooks/jenkins"
+  }
+  provisioner "file" {
+          source      = "${var.cookbooksDir}/jenkins/files/default/"
+          destination = "~/cookbooks/jenkins/files"
+  }
+  provisioner "file" {
+          source      = "${var.cookbooksDir}/jenkins/files/jobs/"
+          destination = "~/cookbooks/jenkins/files"
+  }
+  provisioner "file" {
+          source      = "${var.cookbooksDir}/jenkins/files/node/"
+          destination = "~/cookbooks/jenkins/files"
+  }
+  provisioner "file" {
+          source      = "${var.cookbooksDir}/jenkins/files/scriptapproval/"
+          destination = "~/cookbooks/jenkins/files"
+  }
+  provisioner "file" {
+          source      = "${var.cookbooksDir}/jenkins/files/credentials/"
+          destination = "~/cookbooks/jenkins/files"
+  }
+  provisioner "file" {
+          source      = "${var.cookbooksDir}/jenkins/attributes/"
+          destination = "~/cookbooks/jenkins/"
+  }
+  provisioner "file" {
+          source      = "${var.cookbooksDir}/jenkins/attributes/"
+          destination = "~/cookbooks/blankJenkins/attributes/"
+  }
+  provisioner "file" {
+          source      = "jazz-core/aws-apigateway-importer/"
+          destination = "/tmp"
+  }
+
+ provisioner "remote-exec" {
+    inline = [
+          "sudo chef-client --local-mode -c ~/chefconfig/client.rb --override-runlist blankJenkins::configureblankjenkins"
     ]
   }
 
