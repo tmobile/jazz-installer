a65f3dba5b0188a2f4be832e50be9e816d26dea2
jenkins1 <harin.jose@ust-global.com>
2017-10-28 16:57:20 +0000

    updating maven version
    Former-commit-id: bf0d4ed2d6e64700b558cfcc0d16f880b1dadf7f

--- installscripts/cookbooks/maven/recipes/default.rb
+++ installscripts/cookbooks/maven/recipes/default.rb
@@ -7,21 +7,21 @@
 # All rights reserved - Do Not Redistribute
 #
 
-remote_file '/tmp/apache-maven-3.5.0-bin.tar.gz' do
-  source 'http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz'
+remote_file '/tmp/apache-maven-3.5.2-bin.tar.gz' do
+  source 'http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz'
   mode '0755'
   action :create
 end
 execute 'install_maven' do
-  command 'tar xzvf /tmp/apache-maven-3.5.0-bin.tar.gz'
+  command 'tar xzvf /tmp/apache-maven-3.5.2-bin.tar.gz'
   cwd '/opt'
 end
 
 file '/etc/profile.d/maven.sh' do
-  content "export PATH=$PATH:/opt/apache-maven-3.5.0/bin"
+  content "export PATH=$PATH:/opt/apache-maven-3.5.2/bin"
 end
 execute 'createsymlink' do
-  command 'sudo ln -sf /opt/apache-maven-3.5.0/bin/mvn /usr/bin/mvn'
+  command 'sudo ln -sf /opt/apache-maven-3.5.2/bin/mvn /usr/bin/mvn'
 end
 
 
