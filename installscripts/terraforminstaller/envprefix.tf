c2813da999aa9f66773cae80657350d30ca2a060
Raghavendra Pai <U39592@UST-PCI.COM>
2017-11-01 12:26:24 +0530

    Jazz Installer Cleaup: Removed not used files, variables.
    Former-commit-id: 77a56bf348f9118657277ddf77abaeb690485fc3

--- installscripts/terraforminstaller/envprefix.tf
+++
@@ -1,5 +0,0 @@
-variable "envPrefix" { type = "string" default = "replace here" }
-variable "tagsApplication" { type = "string" default = "JAZZ" }
-variable "tagsEnvironment" { type = "string" default = "Development" }
-variable "tagsExempt" { type = "string" default = "09/01/2017" }
-variable "tagsOwner" { type = "string" default = "replace here" }
