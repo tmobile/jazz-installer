8a64124d5aad31c726932591872a446d0080a166
Feroz Shaikh <feroz.shaikh@ust-global.com>
2017-11-07 09:47:41 +0530

    Added user provided stack name into serverless-config-pack

--- installscripts/terraform-unix-noinstances-jazz/provisioners.tf
+++ installscripts/terraform-unix-noinstances-jazz/provisioners.tf
@@ -3,11 +3,11 @@ resource "null_resource" "configureExistingJenkinsServer" {
   depends_on = ["aws_api_gateway_rest_api.jazz-dev","aws_s3_bucket.jazz-web","aws_iam_role.lambda_role","aws_elasticsearch_domain.elasticsearch_domain" ]
 
   connection {
-    host = "${lookup(var.jenkinsservermap, "jenkins_public_ip")}" 
+    host = "${lookup(var.jenkinsservermap, "jenkins_public_ip")}"
     user = "${lookup(var.jenkinsservermap, "jenkins_ssh_login")}"
     type = "ssh"
     private_key = "${file("${lookup(var.jenkinsservermap, "jenkins_ssh_key")}")}"
-  }   
+  }
   provisioner "local-exec" {
     command = "${var.configureJenkinselb_cmd} ${lookup(var.jenkinsservermap, "jenkins_elb")} ${var.jenkinsattribsfile} ${var.bitbucketclient_cmd}"
   }
@@ -21,7 +21,7 @@ resource "null_resource" "configureExistingJenkinsServer" {
   provisioner "file" {
           source      = "${var.chefconfigDir}"
           destination = "~/chefconfig"
-  } 
+  }
 
  provisioner "remote-exec" {
     inline = [
@@ -39,7 +39,7 @@ resource "null_resource" "configureExistingJenkinsServer" {
           source      = "${var.cookbooksDir}"
           destination = "~/cookbooks"
   }
- 
+
   provisioner "file" {
           source      = "${var.cookbooksDir}/jenkins/recipes"
           destination = "~/cookbooks/jenkins"
@@ -85,7 +85,7 @@ resource "null_resource" "configureExistingJenkinsServer" {
   }
 
   provisioner "local-exec" {
-    command = "${var.modifyCodebase_cmd}  ${lookup(var.jenkinsservermap, "security_group")} ${lookup(var.jenkinsservermap, "subnet")} ${aws_iam_role.lambda_role.arn} ${var.region}"
+    command = "${var.modifyCodebase_cmd}  ${lookup(var.jenkinsservermap, "security_group")} ${lookup(var.jenkinsservermap, "subnet")} ${aws_iam_role.lambda_role.arn} ${var.region} ${var.envPrefix}"
   }
 
 }
@@ -94,11 +94,11 @@ resource "null_resource" "configureExistingBitbucketServer" {
   depends_on = ["null_resource.configureExistingJenkinsServer","aws_elasticsearch_domain.elasticsearch_domain"]
 
   connection {
-    host = "${lookup(var.bitbucketservermap, "bitbucket_public_ip")}" 
+    host = "${lookup(var.bitbucketservermap, "bitbucket_public_ip")}"
     user = "${lookup(var.bitbucketservermap, "bitbucket_ssh_login")}"
     type = "ssh"
     private_key = "${file("${lookup(var.bitbucketservermap, "bitbucket_ssh_key")}")}"
-  }   
+  }
   provisioner "file" {
           source      = "${var.chefconfigDir}/bitbucketelbconfig.json"
           destination = "~/chefconfig/bitbucketelbconfig.json"
