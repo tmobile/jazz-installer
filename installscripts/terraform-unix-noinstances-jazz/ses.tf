811517e0ea246e2fad95017d5e2c2b681a1fecf7
Feroz-Shaikh <32950111+Feroz-Shaikh@users.noreply.github.com>
2017-11-08 17:30:18 +0530

    Added cognito dependency
    Former-commit-id: 0bcca3db00d41413db20a453c01bac884fb7d970

--- installscripts/terraform-unix-noinstances-jazz/ses.tf
+++ installscripts/terraform-unix-noinstances-jazz/ses.tf
@@ -1,4 +1,5 @@
 resource "null_resource" "ses_setup" {
+depends_on = ["null_resource.cognito_user_pool"]
 
 provisioner "local-exec" {
     command = "${var.ses_cmd} ${var.cognito_pool_username} ${var.region} ${var.jenkinsattribsfile} ${var.aws_access_key} ${var.aws_secret_key}"
