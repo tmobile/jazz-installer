d1c7b11567b3a60086edaf1f2918a0b98bc99f0b
Raghavendra Pai <raghavendra.pai@ust-global.com>
2017-11-12 15:04:54 +0530

    Removing public permissions from 5 s3 buckets.
    Former-commit-id: 7ab1a113de53ee0325de7b5860d683c162dc7a3f

--- installscripts/terraform-unix-noinstances-jazz/scripts/deployS3Webapp.sh
+++ installscripts/terraform-unix-noinstances-jazz/scripts/deployS3Webapp.sh
@@ -7,7 +7,7 @@ aws s3 cp . s3://$BUCKET_NAME  --recursive --region $REGION
 IFS=$(echo -en "\n\b")
 for key in $( find . \( -not -type d \) -print | sed 's/^.\///g' ); do
     echo item: $key
-         aws s3api put-object-acl --bucket $BUCKET_NAME --key $key --grant-full-control id=$CANONICAL_ID,uri=http://acs.amazonaws.com/groups/s3/LogDelivery,uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers --grant-read uri=http://acs.amazonaws.com/groups/global/AllUsers   --grant-read-acp uri=http://acs.amazonaws.com/groups/global/AllUsers
+         aws s3api put-object-acl --bucket $BUCKET_NAME --key $key --grant-full-control id=$CANONICAL_ID,uri=http://acs.amazonaws.com/groups/s3/LogDelivery
 done
 cd -
 
