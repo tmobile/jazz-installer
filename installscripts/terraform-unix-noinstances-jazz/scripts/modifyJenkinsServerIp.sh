89ff355fec02845e949335725cfa8707588ce6bf
Raghavendra Pai <raghavendra.pai@ust-global.com>
2017-11-15 12:34:49 +0530

    Jazz OSS Cleanup: unwanted files folders
    Former-commit-id: e4e67a59ebc6721071deb767d9863e81d5931938

--- installscripts/terraform-unix-noinstances-jazz/scripts/modifyJenkinsServerIp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -i "s/default\['server'\]\['privateip'\].*.$/default['server']['privateip']='$1'/g" $2
--- installscripts/terraform-unix-noinstances-jazz/scripts/modifyPropertyFile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-property_key=$1
-property_value=$2
-property_file=$3
-property_value=$(sed 's/[]\/$*.^|[]/\\&/g' <<< $property_value)
-
-sed -i "s/$property_key.*.$/$property_key=$property_value/g" $property_file
-
