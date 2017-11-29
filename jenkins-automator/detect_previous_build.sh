3e9724916767bc62b1eb2f822974104e67366159
Feroz Shaikh <feroz.shaikh@ust-global.com>
2017-11-29 10:19:47 +0530

    updated destroy script
    Former-commit-id: 1b8e80f5282986b326ef2fec24b106c35b3a7ec1

--- jenkins-automator/detect_previous_build.sh
+++ jenkins-automator/detect_previous_build.sh
@@ -4,23 +4,24 @@ if [ ! -f $check_file ]; then
     echo "No previous stack_creation logs found!"
     echo "Proceeding with new stack deployment..."
 else
-    for i in {0..30}
-    do
-        check_date=`date -d "-$i day" +%Y%m%d`
-        stack_name="jazz$check_date"
-        command=`grep -m 2 -i $stack_name $check_file`
-        if [ $? -eq 0 ] ; then
-            echo "Found stack_name: $stack_name in $check_file"
-            echo "Destroying stack $stack_name...."
-            if [ -f "~/jazz-installer/destroy.sh" ]; then
+    if [ ! -f "~/jazz-installer/destroy.sh" ]; then
+        echo "Destroy script not found!"
+        exit
+    else
+        for i in {0..30}
+        do
+            check_date=`date -d "-$i day" +%Y%m%d`
+            stack_name="jazz$check_date"
+            command=`grep -m 2 -i $stack_name $check_file`
+            if [ $? -eq 0 ] ; then
+                echo "Found stack_name: $stack_name in $check_file"
+                echo "Destroying stack $stack_name...."
                 ~/jazz-installer/destroy.sh all
-                else
-                echo "Destroy script not found!"
+                exit
             fi
-            exit
+        done
+        if [ $? -eq 0 ]; then
+            echo "No stacks found in $check_file"
         fi
-    done
-    if [ $? -eq 0 ]; then
-        echo "No stacks found in $check_file"
     fi
 fi
