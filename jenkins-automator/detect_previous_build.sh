3f3ca0c6dffda22f834d3947b415d2ee1554ce4f
Feroz Shaikh <feroz.shaikh@ust-global.com>
2017-11-29 18:47:14 +0530

    updated destroy stack logic
    Former-commit-id: 5df57194b135249cd9be505c52645e6309e779c6

--- jenkins-automator/detect_previous_build.sh
+++ jenkins-automator/detect_previous_build.sh
@@ -1,12 +1,11 @@
 check_file="/home/ec2-user/jazz-installer/stack_creation.out"
 
-if [ ! -f $check_file ]; then
+if [ ! -e $check_file ]; then
     echo "No previous stack_creation logs found!"
     echo "Proceeding with new stack deployment..."
 else
-    if [ ! -f "~/jazz-installer/destroy.sh" ]; then
+    if [ ! -e "/home/ec2-user/jazz-installer/destroy.sh" ]; then
         echo "Destroy script not found!"
-        exit
     else
         for i in {0..30}
         do
