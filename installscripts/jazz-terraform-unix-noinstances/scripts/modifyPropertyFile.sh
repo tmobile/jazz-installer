8f6154932e392fdefa12ee3860d886ab64803a44
Sooraj <sooraj.sasidharannair@ust-global.com>
2017-12-19 15:40:59 +0530

    updates on migrating installer configuration to private repo

--- installscripts/jazz-terraform-unix-noinstances/scripts/modifyPropertyFile.sh
+++ installscripts/jazz-terraform-unix-noinstances/scripts/modifyPropertyFile.sh
@@ -3,11 +3,20 @@
 property_key=$1
 property_value=$2
 property_file=$3
-property_file_json=
+
+
 property_value=$(sed 's/[]\/$*.^|[]/\\&/g' <<< $property_value)
 
-sed -i "s/$property_key.*.$/$property_key=$property_value/g" $property_file
+if [ '$3' == "jazz-installer-vars.json" ] ;
+        then
+        sed -i "s/$property_key\".*.$/$property_key\": \"$property_value\"/g" $property_file
+elif [ '$3' == "jenkins-conf.properties" ] ;
+        then
+        sed -i "s/$property_key.*.$/$property_key=$property_value/g" $property_file
+else
+        echo "file not found"
+fi
 
-#JSON Format
-sed -i "s/$property_key\".*.$/$property_key\": \"$property_value\"/g" $property_file_json
+
+
 
