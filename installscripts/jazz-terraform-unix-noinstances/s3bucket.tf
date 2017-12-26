a8cb20bb1450689e90a980724bc9168796c40e55
Sooraj <sooraj.sasidharannair@ust-global.com>
2017-12-26 11:25:13 +0530

    updates on migrating installer configuration to private repo

--- installscripts/jazz-terraform-unix-noinstances/s3bucket.tf
+++ installscripts/jazz-terraform-unix-noinstances/s3bucket.tf
@@ -140,7 +140,7 @@ resource "aws_api_gateway_rest_api" "jazz-stag" {
 
 resource "aws_api_gateway_rest_api" "jazz-prod" {
   name        = "${var.envPrefix}-prod"
-  description = "PROD API for Tmobile demo"
+  description = "PROD API"
   provisioner "local-exec" {
     command = "git clone -b ${var.github_branch} https://github.com/tmobile/jazz.git jazz-core"
   }
