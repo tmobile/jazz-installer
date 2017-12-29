4fc4b30ce733c1d3a3dce996c3f61c547dd9d862
Sini Wilson <Sini.Wilson@ust-global.com>
2017-12-29 11:22:10 +0530

    Changing job url

--- installscripts/jazz-terraform-unix-noinstances/s3bucket.tf
+++ installscripts/jazz-terraform-unix-noinstances/s3bucket.tf
@@ -142,7 +142,7 @@ resource "aws_api_gateway_rest_api" "jazz-prod" {
   name        = "${var.envPrefix}-prod"
   description = "PROD API"
   provisioner "local-exec" {
-    command = "git clone -b ${var.github_branch} https://github.com/tmobile/jazz.git jazz-core"
+   command = "git clone -b ${var.github_branch} https://github.com/SiniWilson/jazz.git jazz-core"
   }
   provisioner "local-exec" {
     command = "${var.configureApikey_cmd} ${aws_api_gateway_rest_api.jazz-dev.id} ${aws_api_gateway_rest_api.jazz-stag.id} ${aws_api_gateway_rest_api.jazz-prod.id} ${var.region} ${var.jenkinspropsfile}  ${var.jenkinsattribsfile} ${var.envPrefix}"
