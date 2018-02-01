7601a2e6700b772014f1a36c80b057e273e3f432
SiniWilson <sini.wilson@ust-global.com>
2018-02-01 16:05:41 +0530

    CAPI-1212:Migrate installer configuration to private repo - Part 2
    Former-commit-id: a491e4b25cf31e55f7e63f1b54c467b28ef7c92c

---
+++ installscripts/jazz-terraform-unix-noinstances/scripts/configureSubent.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+securityGroupIds=$1
+subnetIds=$2
+stackprefix=$3
+jenkinsjsonpropsfile=$4
+role="_lambda2_basic_execution_1"
+iam=$stackprefix$role
+
+
+sed -i "s/SECURITY_GROUP_IDS\".*.$/SECURITY_GROUP_IDS\": \[$securityGroupIds\],/g" $jenkinsjsonpropsfile
+sed -i "s/SUBNET_IDS\".*.$/SUBNET_IDS\": \[$subnetIds\],/g" $jenkinsjsonpropsfile
+sed -i "s/LAMBDA_EXECUTION_ROLE\".*.$/LAMBDA_EXECUTION_ROLE\": \"$iam\"/g" $jenkinsjsonpropsfile
