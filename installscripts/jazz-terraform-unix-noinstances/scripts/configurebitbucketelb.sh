060689c716d150f2055af4922791dde0d392f246
Surya Jakhotia <suryaj@gmail.com>
2018-02-01 23:28:12 -0800

    Revert "V1.4 : Rebasing V1.4 from master"

--- installscripts/jazz-terraform-unix-noinstances/scripts/configurebitbucketelb.sh
+++ installscripts/jazz-terraform-unix-noinstances/scripts/configurebitbucketelb.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-scmbb=$1
-bitbucketelbdnsname=$2
-jenkinsattribsfile=$3
-jenkinspropertiesfile=$4
-jenkinsjsonpropertiesfile=$5
-bitbucketclient=$6
-inst_stack_prefix=$7
-jazz_admin=$8
 
-#proceeding
-if [ "$scmbb" == 1 ]
-then
-    sed -i "s/default\['scmelb'\].*.$/default['scmelb']='$bitbucketelbdnsname'/g"  $jenkinsattribsfile
-    sed -i "s/REPO_BASE=.*.$/REPO_BASE=$bitbucketelbdnsname/g" $jenkinspropertiesfile
-    sed -i "s/REPO_BASE\".*.$/REPO_BASE\": \"$bitbucketelbdnsname\",/g" $jenkinsjsonpropertiesfile
-    sed -i "s/BITBUCKETELB=.*.$/BITBUCKETELB=$bitbucketelbdnsname/g" $bitbucketclient
-fi
+bitbucketelb_dns_name=$1
+jenkinsattribsfile=$2
+jenkinsjsonpropertiesfile=$3
+bitbucketclient=$4
+inst_stack_prefix=$5
+jazz_admin=$6
+
+sed -i "s/default\['bitbucketelb'\].*.$/default['bitbucketelb']='$bitbucketelb_dns_name'/g"  $jenkinsattribsfile
+sed -i "s/REPO_BASE\".*.$/REPO_BASE\": \"$bitbucketelb_dns_name\",/g" $jenkinsjsonpropertiesfile
+
+
+sed -i "s/BITBUCKETELB=.*.$/BITBUCKETELB=$bitbucketelb_dns_name/g" $bitbucketclient
+
+#Modify platform_services config files
+sed -i 's/"services_table": ".*.$/"services_table": "'$inst_stack_prefix'_services_dev",/g' ./jazz-core/platform_services/config/dev-config.json
+sed -i 's/"services_table": ".*.$/"services_table": "'$inst_stack_prefix'_services_stg",/g' ./jazz-core/platform_services/config/stg-config.json
+sed -i 's/"services_table": ".*.$/"services_table": "'$inst_stack_prefix'_services_prod",/g' ./jazz-core/platform_services/config/prod-config.json
+
+sed -i 's/"admin_users": ".*.$/"admin_users": "'$jazz_admin'"/g' ./jazz-core/platform_services/config/dev-config.json
+sed -i 's/"admin_users": ".*.$/"admin_users": "'$jazz_admin'"/g' ./jazz-core/platform_services/config/stg-config.json
+sed -i 's/"admin_users": ".*.$/"admin_users": "'$jazz_admin'"/g' ./jazz-core/platform_services/config/prod-config.json
--- installscripts/wizard/scenarios/__init__.py
+++ installscripts/wizard/scenarios/__init__.py
@@ -1,4 +1,3 @@
 from stack_with_bb_jenkins import *
 from stack_with_bb_dockerized_jenkins import *
-from stack_with_dockerized_gitlab_jenkins import *
-
+from full_stack import *
