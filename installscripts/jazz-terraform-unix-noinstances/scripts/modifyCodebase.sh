2bea70f36fecb6a26a969d2ee3b38d322eaf5ec4
SiniWilson <sini.wilson@ust-global.com>
2018-02-05 14:20:22 +0530

    CAPI-1210: Fixing runtime issue
    Former-commit-id: c6da6e138f1f558e211fa6d0c2ccaad8f3c01097

--- installscripts/jazz-terraform-unix-noinstances/scripts/modifyCodebase.sh
+++ installscripts/jazz-terraform-unix-noinstances/scripts/modifyCodebase.sh
@@ -6,6 +6,14 @@ iamRoleARN=$3
 region=$4
 stackprefix=$5
 jazz_admin=$6
+role=__lambda2_basic_execution_1
+iam=$stackprefix$role
+
+JENKINS_JSON_PROPFILE=/home/$SSH_USER/cookbooks/jenkins/files/node/jazz-installer-vars.json
+
+sed -i "s/AWS.SECURITY_GROUP_IDS\".*.$/AWS.SECURITY_GROUP_IDS\": \"$securityGroupIds\",/g" $JENKINS_JSON_PROPFILE
+sed -i "s/AWS.SUBNET_IDS\".*.$/AWS.SUBNET_IDS\": \"$subnetIds\",/g" $JENKINS_JSON_PROPFILE
+sed -i "s/AWS.LAMBDA_EXECUTION_ROLE\".*.$/AWS.LAMBDA_EXECUTION_ROLE\": \"$iam\"/g" $JENKINS_JSON_PROPFILE
 
 sed -i "s/^securityGroupIds.*.$/securityGroupIds: $securityGroupIds/g" ./jazz-core/api-template-java/deployment-env.yml
 sed -i "s/^subnetIds.*.$/subnetIds: $subnetIds/g" ./jazz-core/api-template-java/deployment-env.yml
@@ -43,30 +51,56 @@ sed -i "s/{inst_stack_prefix}/$stackprefix/g" ./jazz-core/serverless-config-pack
 sed -i "s/{inst_stack_prefix}/$stackprefix/g" ./jazz-core/serverless-config-pack/serverless-python.yml
 
 #-------------------------------------------
-platform_services=("cognito-authorizer" "logs" "usermanagement" "services-handler" "events" "services" "logout" "login" "cloud-logs-streamer" "is-service-available" "delete-serverless-service" "create-serverless-service" "email" )
+platform_services=("cognito-authorizer" "platform_logs" "platform_usermanagement" "platform-services-handler" "platform_events" "platform_services" "platform_logout" "platform_login" "cloud-logs-streamer" "is-service-available" "delete-serverless-service" "create-serverless-service" "platform_email" )
 servicename="_services_prod"
 tablename=$stackprefix$servicename
 for element in "${platform_services[@]}"
 do
-	uuid=`uuidgen -t`
-	aws dynamodb put-item --table-name $tablename --item '{
-		"SERVICE_ID":{"S":'$uuid'},
-		"SERVICE_CREATED_BY":{"S":'$jazz_admin'},
-		"SERVICE_DOMAIN":{"S":"platform"},
-		"SERVICE_NAME":{"S":'$element'},
-		"SERVICE_RUNTIME":{"S":"nodejs"},
-		"SERVICE_STATUS":{"S":"active"},
-		"SERVICE_METADATA":{"M":{
-				"securityGroupIds":{"S":'$securityGroupIds'},
-				"subnetIds":{"S":'$subnetIds'},
-				"iamRoleARN":{"S":'$iamRoleARN'},
-				"providerMemorySize":{"S":"256"},
-				"providerRuntime":{"S":"nodejs4.3"},
-				"providerTimeout":{"S":"160"},
-				"runtime":{"S":"nodejs"},
-				"service":{"S":'$element'},
-				"type":{"S":"api"}
-				}
-			}
-		}'
+  uuid=`uuidgen -t`
+  echo -n > ./jazz-core/$element/deployment-env.yml
+  echo "service_id: "$uuid >> ./jazz-core/$element/deployment-env.yml
+ 
+  if [ element != "platform_email" ] ; then	  
+	  aws dynamodb put-item --table-name $tablename --item '{
+			  "SERVICE_ID":{"S":"'$uuid'"},
+			  "SERVICE_CREATED_BY":{"S":"'$jazz_admin'"},
+			  "SERVICE_DOMAIN":{"S":"platform"},
+			  "SERVICE_NAME":{"S":"'$element'"},
+			  "SERVICE_RUNTIME":{"S":"nodejs"},
+			  "SERVICE_STATUS":{"S":"active"},
+			  "SERVICE_METADATA":{"M":{
+							  "securityGroupIds":{"S":"'$securityGroupIds'"},
+							  "subnetIds":{"S":"'$subnetIds'"},
+							  "iamRoleARN":{"S":"'$iamRoleARN'"},
+							  "providerMemorySize":{"S":"256"},
+							  "providerRuntime":{"S":"nodejs4.3"},
+							  "providerTimeout":{"S":"160"},
+							  "runtime":{"S":"nodejs"},
+							  "service":{"S":"'$element'"},
+							  "type":{"S":"api"}
+							  }
+					  }
+			  }'
+	else 
+		aws dynamodb put-item --table-name $tablename --item '{
+			  "SERVICE_ID":{"S":"'$uuid'"},
+			  "SERVICE_CREATED_BY":{"S":"'$jazz_admin'"},
+			  "SERVICE_DOMAIN":{"S":"platform"},
+			  "SERVICE_NAME":{"S":"'$element'"},
+			  "SERVICE_RUNTIME":{"S":"nodejs"},
+			  "SERVICE_STATUS":{"S":"active"},
+			  "SERVICE_METADATA":{"M":{
+							  "securityGroupIds":{"S":"'$securityGroupIds'"},
+							  "subnetIds":{"S":"'$subnetIds'"},
+							  "iamRoleARN":{"S":"'$iamRoleARN'"},
+							  "providerMemorySize":{"S":"256"},
+							  "providerRuntime":{"S":"nodejs6.10"},
+							  "providerTimeout":{"S":"160"},
+							  "runtime":{"S":"nodejs"},
+							  "service":{"S":"'$element'"},
+							  "type":{"S":"api"}
+							  }
+					  }
+			  }'
+   fi
 done
