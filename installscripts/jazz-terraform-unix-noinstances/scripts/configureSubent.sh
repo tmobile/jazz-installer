f931f5e8c51aad584e8c6b677031c5aac05edbcc
SiniWilson <sini.wilson@ust-global.com>
2018-02-08 14:48:43 +0530

    Rebased 08/02/2018
    Former-commit-id: f5af836ca92e29922389e023c9220189df2a84ac

--- installscripts/jazz-terraform-unix-noinstances/scripts/configureSubent.sh
+++ installscripts/jazz-terraform-unix-noinstances/scripts/configureSubent.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Expecting Sg and subnetIds are coming as comma seperated
 securityGroupIds=$1
 subnetIds=$2
 stackprefix=$3
@@ -7,24 +8,6 @@ jenkinsjsonpropsfile=$4
 role="_lambda2_basic_execution_1"
 iam=$stackprefix$role
 
-secGpIds="\""
-sbIds="\""
-quote="\",\""
-
-for element in "${securityGroupIds[@]}"
-do
- secGpIds=$secGpIds$element$quote
-done
-
-for element in "${subnetIds[@]}"
-do
-  sbIds=$sbIds$element$quote
-done
-
-sbIds=${sbIds::-2}
-secGpIds=${secGpIds::-2}
-
-
-sed -i "s/SECURITY_GROUP_IDS\".*.$/SECURITY_GROUP_IDS\": \[$secGpIds\],/g" $jenkinsjsonpropsfile
-sed -i "s/SUBNET_IDS\".*.$/SUBNET_IDS\": \[$sbIds\],/g" $jenkinsjsonpropsfile
+sed -i "s/SECURITY_GROUP_IDS\".*.$/SECURITY_GROUP_IDS\": \"$securityGroupIds\",/g" $jenkinsjsonpropsfile
+sed -i "s/SUBNET_IDS\".*.$/SUBNET_IDS\": \"$subnetIds\",/g" $jenkinsjsonpropsfile
 sed -i "s/LAMBDA_EXECUTION_ROLE\".*.$/LAMBDA_EXECUTION_ROLE\": \"$iam\"/g" $jenkinsjsonpropsfile
