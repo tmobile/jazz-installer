345706d3faafa4c7f87e54048ef776ef04159f23
SiniWilson <sini.wilson@ust-global.com>
2018-02-09 15:17:53 +0530

    US273818: OSS - Installer - Simplify:: Remove cloud front logs from Install
    Former-commit-id: 9c62f63bbbf8d2efe38e3409fd91bbf7b5733089

--- installscripts/jazz-terraform-unix-noinstances/scripts/configureS3Names.sh
+++ installscripts/jazz-terraform-unix-noinstances/scripts/configureS3Names.sh
@@ -3,12 +3,10 @@
 jazz_bucket_dev=$1
 jazz_bucket_stg=$2
 jazz_bucket_prod=$3
-jazz_bucket_cloudfrontlogs=$4
-jazz_bucket_web=$5
-jenkinsjsonpropsfile=$6
+jazz_bucket_web=$4
+jenkinsjsonpropsfile=$5
 
 sed -i "s/\"DEV_BUCKET\".*\"\"/\"DEV_BUCKET\" : \"$jazz_bucket_dev\"/g" $jenkinsjsonpropsfile
 sed -i "s/\"STG_BUCKET\".*\"\"/\"STG_BUCKET\" : \"$jazz_bucket_stg\"/g" $jenkinsjsonpropsfile
 sed -i "s/\"PROD_BUCKET\".*\"\"/\"PROD_BUCKET\" : \"$jazz_bucket_prod\"/g" $jenkinsjsonpropsfile
-sed -i "s/\"CFLOGS_BUCKET\".*\"\"/\"CFLOGS_BUCKET\" : \"$jazz_bucket_cloudfrontlogs\"/g" $jenkinsjsonpropsfile
 sed -i "s/\"S3_BUCKET_WEB\".*\"\"/\"S3_BUCKET_WEB\" : \"$jazz_bucket_web\"/g" $jenkinsjsonpropsfile
