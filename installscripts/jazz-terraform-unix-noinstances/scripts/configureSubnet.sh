980edc09f9381cedfa4125b3f4d72cc850553268
SiniWilson <sini.wilson@ust-global.com>
2018-02-12 18:36:37 +0530

    Fixing json issue

--- installscripts/jazz-terraform-unix-noinstances/scripts/configureSubnet.sh
+++ installscripts/jazz-terraform-unix-noinstances/scripts/configureSubnet.sh
@@ -11,3 +11,4 @@ iam=$stackprefix$role
 sed -i "s/\"SECURITY_GROUP_IDS\".*\"\"/\"SECURITY_GROUP_IDS\" : \"$securityGroupIds\"/g" $property_file_json
 sed -i "s/\"SUBNET_IDS\".*\"\"/\"SUBNET_IDS\" : \"$subnetIds\"/g" $property_file_json
 sed -i "s/\"LAMBDA_EXECUTION_ROLE\".*\"\"/\"LAMBDA_EXECUTION_ROLE\" : \"$iam\"/g" $property_file_json
+
