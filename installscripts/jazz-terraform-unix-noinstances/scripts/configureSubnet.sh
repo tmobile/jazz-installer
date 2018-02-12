2120cb03c2284fe7df87c67a92b6ab03f61a2cc9
SiniWilson <sini.wilson@ust-global.com>
2018-02-12 18:43:41 +0530

    Follo up PR : Fixing json issue
    Former-commit-id: abf440ce569a3aa2568c905a5180b3c2d9269d3a

--- installscripts/jazz-terraform-unix-noinstances/scripts/configureSubnet.sh
+++ installscripts/jazz-terraform-unix-noinstances/scripts/configureSubnet.sh
@@ -8,7 +8,7 @@ jenkinsjsonpropsfile=$4
 role="_lambda2_basic_execution_1"
 iam=$stackprefix$role
 
-sed -i "s/\"SECURITY_GROUP_IDS\".*\"\"/\"SECURITY_GROUP_IDS\" : \"$securityGroupIds\"/g" $property_file_json
-sed -i "s/\"SUBNET_IDS\".*\"\"/\"SUBNET_IDS\" : \"$subnetIds\"/g" $property_file_json
-sed -i "s/\"LAMBDA_EXECUTION_ROLE\".*\"\"/\"LAMBDA_EXECUTION_ROLE\" : \"$iam\"/g" $property_file_json
+sed -i "s/SECURITY_GROUP_IDS\".*.$/SECURITY_GROUP_IDS\": \"$securityGroupIds\",/g" $jenkinsjsonpropsfile
+sed -i "s/SUBNET_IDS\".*.$/SUBNET_IDS\": \"$subnetIds\",/g" $jenkinsjsonpropsfile
+sed -i "s/LAMBDA_EXECUTION_ROLE\".*.$/LAMBDA_EXECUTION_ROLE\": \"$iam\",/g" $jenkinsjsonpropsfile
 
