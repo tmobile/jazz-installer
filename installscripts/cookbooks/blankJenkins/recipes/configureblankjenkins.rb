b1996ce33f73cb3d271af116ca59e867fdba3e84
SiniWilson <sini.wilson@ust-global.com>
2018-02-22 16:37:25 +0530

    [WIP] DE134260: Removing gitlab new job
    Former-commit-id: 81f802ad4a55a507d31c4cb79d9eff8ac7b0723e

--- installscripts/cookbooks/blankJenkins/recipes/configureblankjenkins.rb
+++ installscripts/cookbooks/blankJenkins/recipes/configureblankjenkins.rb
@@ -116,10 +116,6 @@ if node[:platform_family].include?("rhel")
       only_if  { node[:scm] == 'gitlab' }
       command "/home/#{node['jenkins']['SSH_user']}/jenkins/files/jobs/job-gitlab-trigger.sh #{node['jenkinselb']} #{node['jenkins']['SSH_user']} #{node['scmpath']}"
     end
-	execute 'job-trigger-platform-services-build' do
-      only_if  { node[:scm] == 'gitlab' }
-      command "/home/#{node['jenkins']['SSH_user']}/jenkins/files/jobs/job-trigger-platform-services-build.sh #{node['jenkinselb']} #{node['jenkins']['SSH_user']} #{node['scmpath']}"
-    end
     link '/usr/bin/aws-api-import' do
       to "/home/#{node['jenkins']['SSH_user']}/jazz-core/aws-apigateway-importer/aws-api-import.sh"
       owner 'jenkins'
@@ -251,10 +247,6 @@ if node[:platform_family].include?("debian")
     execute 'job-gitlab-trigger' do
       only_if  { node[:scm] == 'gitlab' }
       command "/root/cookbooks/jenkins/files/jobs/job-gitlab-trigger.sh #{node['jenkinselb']} root #{node['scmpath']}"
-    end
-	execute 'job-trigger-platform-services-build' do
-      only_if  { node[:scm] == 'gitlab' }
-      command "/root/cookbooks/jenkins/files/jobs/job-trigger-platform-services-build.sh #{node['jenkinselb']} root #{node['scmpath']}"
     end
     link '/usr/bin/aws-api-import' do
       to "/root/jazz-core/aws-apigateway-importer/aws-api-import.sh"
