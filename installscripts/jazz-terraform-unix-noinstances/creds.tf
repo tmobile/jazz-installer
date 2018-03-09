b55580bade1382b0a23a013819e436ca8aa0fc5d
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-03-08 16:05:54 -0800

    Pass AWS creds via Terraform CLI to decouple TF from setup scripts
    Former-commit-id: 82fe6176abc470eab6006aeb49900fae96227bf5

--- installscripts/jazz-terraform-unix-noinstances/creds.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-    #This ec2-user user is of Installer box
-    shared_credentials_file  = "/home/centos/.aws/credentials"
-    profile                  = "default"
-    region = "${var.region}"
-}
