f8849044f9bd968c77cd4dd4c88c158c14cce823
Feroz-Shaikh <feroz.shaikh@ust-global.com>
2018-03-27 15:02:13 +0530

    Restoring cookbook_path location

--- installscripts/chefconfig/jenkins_client.rb
+++ installscripts/chefconfig/jenkins_client.rb
@@ -1,2 +1,4 @@
 #root = File.absolute_path(File.dirname(__FILE__))
-cookbook_path [ '~/cookbooks' ]
+# Scenario 1 expects cookbook location at /home/centos/cookbooks
+# Scenarios 2 & 3 expects cookbook location at /root/cookbooks
+cookbook_path [ '~/cookbooks', '/home/centos/cookbooks', '/root/cookbooks' ]
