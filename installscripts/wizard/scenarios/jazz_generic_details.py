760741ba8a38c95112da5fd7a14d136080f1b5fc
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-03-29 16:44:02 -0400

    Run flake linter over some of these scripts Ft #110
    Former-commit-id: ec87344aecc43c7021a930a60a39cb22b212c333

--- installscripts/wizard/scenarios/jazz_generic_details.py
+++ installscripts/wizard/scenarios/jazz_generic_details.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python
 import os
-import sys
 import random
 import string
 import subprocess
 import datetime
-from jazz_common import validate_email_id
+from support.jazz_common import validate_email_id
+
 
 def passwd_generator():
     """
         Random password generator for jazz-ui admin email ID login
     """
-    length = 10
     pwd = []
     pwd.append(random.choice(string.ascii_lowercase))
     pwd.append(random.choice(string.ascii_uppercase))
@@ -22,13 +21,16 @@ def passwd_generator():
     random.shuffle(pwd)
     return ''.join(pwd)
 
+
 def get_aws_credentials():
     """
         If the AWS credentials have not already been defined as env vars, populate those env vars
     """
     if "AWS_ACCESS_KEY_ID" and "AWS_SECRET_ACCESS_KEY" not in os.environ:
         os.environ['AWS_ACCESS_KEY_ID'] = raw_input("AWS Access Key ID :")
-        os.environ['AWS_SECRET_ACCESS_KEY'] = raw_input("AWS Secret Access Key :")
+        os.environ['AWS_SECRET_ACCESS_KEY'] = raw_input(
+            "AWS Secret Access Key :")
+
 
 def set_aws_config(region):
     """
@@ -37,22 +39,29 @@ def set_aws_config(region):
     os.environ['AWS_DEFAULT_OUTPUT'] = 'json'
     os.environ['AWS_DEFAULT_REGION'] = region
 
+
 def get_jazz_tag_config_details():
     """
         Get tag configuration details from user and add it to the files
     """
-        #Get the Tag Name from the user - Should not exceed 13 character. It may break the S3 bucket creation
-    tag_env_prefix = raw_input("Please provide a prefix for your stack (limited to 13 characters)(eg: myjazz) :")
-    while(len(tag_env_prefix) > 13 or len(tag_env_prefix) == 0):
-            tag_env_prefix = raw_input("Please provide a prefix for your stack (limited to 13 characters)(eg: myjazz) :")
+    # Get the Tag Name from the user - Should not exceed 13 character. It may break the S3 bucket creation
+    tag_env_prefix = raw_input(
+        "Please provide a prefix for your stack (limited to 13 characters)(eg: myjazz) :"
+    )
+    while (len(tag_env_prefix) > 13 or len(tag_env_prefix) == 0):
+        tag_env_prefix = raw_input(
+            "Please provide a prefix for your stack (limited to 13 characters)(eg: myjazz) :"
+        )
     tag_env_prefix = tag_env_prefix.lower()
 
-    #TODO Since most of these are currently static we could define them with interpolation in envprefix.tf
-    tag_enviornment="Development"
-    tag_exempt=(datetime.datetime.today()+datetime.timedelta(days=1)).strftime("%m/%d/%Y")
-    tag_owner=tag_env_prefix+"-Admin"
+    # TODO Since most of these are currently static we could define them with interpolation in envprefix.tf
+    tag_enviornment = "Development"
+    tag_exempt = (datetime.datetime.today() +
+                  datetime.timedelta(days=1)).strftime("%m/%d/%Y")
+    tag_owner = tag_env_prefix + "-Admin"
 
     return [tag_env_prefix, tag_enviornment, tag_exempt, tag_owner]
+
 
 def get_stack_generic_details(jazz_branch):
     print("")
@@ -63,18 +72,21 @@ def get_stack_generic_details(jazz_branch):
 
     region = raw_input("AWS Region (e.g. us-east-1): ")
     if region not in knownWorkingRegions:
-        print('Warning: This installer has not been tested against the region you specified.\nPlease check the Jazz documentation (https://github.com/tmobile/jazz-installer/wiki#prerequisites) to verify the region you have chosen supports the required AWS resources.')
+        print(
+            'Warning: This installer has not been tested against the region you specified.\nPlease check the Jazz documentation (https://github.com/tmobile/jazz-installer/wiki#prerequisites) to verify the region you have chosen supports the required AWS resources.'
+        )
 
     # Get the aws credentials & set required AWS env vars
-    aws_credentials = get_aws_credentials()
     set_aws_config(region)
 
     # get Jazz Tag details
     jazz_tag_details = get_jazz_tag_config_details()
 
     # Get Cognito details
-    while(1):
-        cognito_email_id = raw_input("Please provide admin email address (will be used to login into Jazz):")
+    while (1):
+        cognito_email_id = raw_input(
+            "Please provide admin email address (will be used to login into Jazz):"
+        )
         if validate_email_id(cognito_email_id):
             break
         else:
@@ -83,16 +95,23 @@ def get_stack_generic_details(jazz_branch):
 
     jazz_account_id = ""
     try:
-        jazz_accountid_cmd = ['aws', 'sts', 'get-caller-identity', '--output', 'text', '--query',
-                              'Account']
+        jazz_accountid_cmd = [
+            'aws', 'sts', 'get-caller-identity', '--output', 'text', '--query',
+            'Account'
+        ]
         jazz_account_id = subprocess.check_output(jazz_accountid_cmd)
 
     except:
-        print("Unable to get caller identity. Are you sure the credentials are correct? Please retry...")
+        print(
+            "Unable to get caller identity. Are you sure the credentials are correct? Please retry..."
+        )
         exit(0)
     jazz_account_id = jazz_account_id[:-1]
 
     # Determine the scenario
-    parameter_list = [jazz_branch, [cognito_email_id, cognito_passwd], jazz_account_id, jazz_tag_details]
+    parameter_list = [
+        jazz_branch, [cognito_email_id, cognito_passwd], jazz_account_id,
+        jazz_tag_details
+    ]
 
     return parameter_list
--- installscripts/wizard/scenarios/stack_with_bb_dockerized_jenkins.py
+++ installscripts/wizard/scenarios/stack_with_bb_dockerized_jenkins.py
@@ -2,28 +2,34 @@
 import os
 import sys
 import subprocess
-from jazz_common import parse_and_replace_parameter_list, INSTALL_SCRIPT_FOLDER, TERRAFORM_FOLDER_PATH, HOME_FOLDER
-from jazz_jenkins import get_and_add_docker_jenkins_config
-from jazz_bitbucket import get_and_add_existing_bitbucket_config
+from support.jazz_common import INSTALL_SCRIPT_FOLDER, TERRAFORM_FOLDER_PATH, parse_and_replace_parameter_list
+from support.jazz_jenkins import get_and_add_docker_jenkins_config
+from support.jazz_bitbucket import get_and_add_existing_bitbucket_config
 
-#Global variables
+# Global variables
 JENKINS_DOCKER_PATH = INSTALL_SCRIPT_FOLDER + "dockerfiles/jenkins/"
 JENKINS_PEM = JENKINS_DOCKER_PATH + "/jenkinskey.pem"
 
+
 def pause():
     programPause = raw_input("Press the <ENTER> key to continue...")
+
 
 def check_dockerised_jenkins_pem():
     """
         Check if the jenkins dockerised has created jenkinskey.pem private keys
     """
-    #Chck if both files are been added to home derectory
+    # Check if both files are been added to home derectory
     if not os.path.isfile(JENKINS_PEM):
         sys.exit("File jenkinskey.pem missing. Aborting! ")
 
-    #Copy the pem keys and give relavant permissions to a dockerkeys location. This is different from Scenario 1.
-    subprocess.call('cp -f {0} {1}sshkeys/dockerkeys'.format(JENKINS_PEM, INSTALL_SCRIPT_FOLDER).split(' '))
-    subprocess.call('sudo chmod 400 {0}sshkeys/dockerkeys/jenkinskey.pem'.format(INSTALL_SCRIPT_FOLDER).split(' '))
+    # Copy the pem keys and give relavant permissions to a dockerkeys Location. This is different from Scenario 1.
+    subprocess.call('cp -f {0} {1}sshkeys/dockerkeys'.format(
+        JENKINS_PEM, INSTALL_SCRIPT_FOLDER).split(' '))
+    subprocess.call(
+        'sudo chmod 400 {0}sshkeys/dockerkeys/jenkinskey.pem'.format(
+            INSTALL_SCRIPT_FOLDER).split(' '))
+
 
 def start(parameter_list):
     """
@@ -39,12 +45,15 @@ def start(parameter_list):
     get_and_add_docker_jenkins_config(JENKINS_DOCKER_PATH)
     check_dockerised_jenkins_pem()
 
-    #All variables are set and ready to call terraform
+    # All variables are set and ready to call terraform
     os.chdir(TERRAFORM_FOLDER_PATH)
 
-    subprocess.call('nohup ./scripts/create.sh >>../../stack_creation.out&',shell=True)
+    subprocess.call(
+        'nohup ./scripts/create.sh >>../../stack_creation.out&', shell=True)
     subprocess.call('cp ./scripts/destroy.sh ../../'.split(' '))
 
-    print("\n\nPlease execute  tail -f stack_creation.out | grep 'Creation complete' in the below directory to see the stack creation progress ")
+    print(
+        "\n\nPlease execute  tail -f stack_creation.out | grep 'Creation complete' in the below directory to see the stack creation progress "
+    )
     print(os.path.realpath('../../'))
     print("\n\n")
