2c60c9cc5f7930faa96020bf4185e7322d9e8925
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-04-04 15:16:38 -0400

    Do `terraform init` and try to fail if TF does

--- installscripts/jazz-terraform-unix-noinstances/scripts/create.sh
+++ installscripts/jazz-terraform-unix-noinstances/scripts/create.sh
@@ -1,20 +1,37 @@
-#!/bin/bash
+#!/bin/sh
+
+print_error()
+{
+    printf "\r${RED}$1${NC}\n" 1>&3 2>&4
+}
+
 rm -f ./settings.txt
 date
-terraform apply \
-          -var "aws_access_key=${AWS_ACCESS_KEY_ID}" \
-          -var "aws_secret_key=${AWS_SECRET_ACCESS_KEY}" \
-          -var "region=${AWS_DEFAULT_REGION}"
+terraform init && terraform apply \
+                            -var "aws_access_key=${AWS_ACCESS_KEY_ID}" \
+                            -var "aws_secret_key=${AWS_SECRET_ACCESS_KEY}" \
+                            -var "region=${AWS_DEFAULT_REGION}"
 date
-echo " ======================================================="
-echo " The following stack has been created in AWS"
-echo " ________________________________________________"
-terraform state list
-echo " ======================================================="
-echo " Please use the following values for checking out Jazz"
-echo " ________________________________________________"
-cat ./settings.txt
-echo " ======================================================="
-echo " Installation complete! To cleanup Jazz stack and its resources execute ./destroy.sh in this directory."
-realpath ../../
-echo " ======================================================="
+if [ $? -gt 0 ]
+then
+    print_error "$message....Failed"
+    echo " ======================================================="
+    echo " To cleanup Jazz stack and its resources execute ./destroy.sh in this directory."
+    realpath ../../
+    echo " ======================================================="
+    exit
+else
+    echo " ======================================================="
+    echo " The following stack has been created in AWS"
+    echo " ________________________________________________"
+    terraform state list
+    echo " ======================================================="
+    echo " Please use the following values for checking out Jazz"
+    echo " ________________________________________________"
+    cat ./settings.txt
+    echo " ======================================================="
+    echo " Installation complete! To cleanup Jazz stack and its resources execute ./destroy.sh in this directory."
+    realpath ../../
+    echo " ======================================================="
+    exit
+fi
