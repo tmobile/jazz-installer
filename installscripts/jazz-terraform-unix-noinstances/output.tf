eb1ff0606cdb70ec5dbbfcd07b15039d5a50f04d
SiniWilson <Sini.Wilson@ust-global.com>
2018-04-11 14:39:21 +0530

    :bug: Fixing quote issue inn stack_details.json

--- installscripts/jazz-terraform-unix-noinstances/output.tf
+++ installscripts/jazz-terraform-unix-noinstances/output.tf
@@ -9,31 +9,31 @@ resource "null_resource" "outputVariables" {
     command = "echo { > stack_details.json"
   }
   provisioner "local-exec" {
-    command = "echo \"jenkins_elb\" : \"http://${lookup(var.jenkinsservermap, "jenkins_elb")}\",>> stack_details.json"
+    command = "echo -e \"jenkins_elb\" : \"http://${lookup(var.jenkinsservermap, "jenkins_elb")}\",>> stack_details.json"
   }
   provisioner "local-exec" {
-    command = "echo \"jenkins_username\" : \"${lookup(var.jenkinsservermap, "jenkinsuser")}\", >> stack_details.json"
+    command = "echo -e \"jenkins_username\" : \"${lookup(var.jenkinsservermap, "jenkinsuser")}\", >> stack_details.json"
   }
   provisioner "local-exec" {
-    command = "echo \"jenkins_password\" : \"${lookup(var.jenkinsservermap, "jenkinspasswd")}\", >> stack_details.json"
+    command = "echo -e \"jenkins_password\" : \"${lookup(var.jenkinsservermap, "jenkinspasswd")}\", >> stack_details.json"
   } 
   provisioner "local-exec" {
-    command = "echo \"jenkins_subnet\" : \"${lookup(var.jenkinsservermap, "jenkins_subnet")}\", >> stack_details.json"
+    command = "echo -e \"jenkins_subnet\" : \"${lookup(var.jenkinsservermap, "jenkins_subnet")}\", >> stack_details.json"
   }
   provisioner "local-exec" {
-    command = "echo \"jazz_home\" : \"http://${aws_cloudfront_distribution.jazz.domain_name}\", >> stack_details.json"
+    command = "echo -e \"jazz_home\" : \"http://${aws_cloudfront_distribution.jazz.domain_name}\", >> stack_details.json"
   }
   provisioner "local-exec" {
-    command = "echo \"jazz_admin_username\" : \"${var.cognito_pool_username}\", >> stack_details.json"
+    command = "echo -e \"jazz_admin_username\" : \"${var.cognito_pool_username}\", >> stack_details.json"
   }
   provisioner "local-exec" {
-    command = "echo \"jazz_admin_password\" : \"${var.cognito_pool_password}\", >> stack_details.json"
+    command = "echo -e \"jazz_admin_password\" : \"${var.cognito_pool_password}\", >> stack_details.json"
   }
   provisioner "local-exec" {
-    command = "echo \"region\" : \"${var.region}\", >> stack_details.json"
+    command = "echo -e \"region\" : \"${var.region}\", >> stack_details.json"
   }
   provisioner "local-exec" {
-    command = "echo \"api_endpoint\" : \"https://${aws_api_gateway_rest_api.jazz-prod.id}.execute-api.${var.region}.amazonaws.com/prod\", >> stack_details.json"
+    command = "echo -e \"api_endpoint\" : \"https://${aws_api_gateway_rest_api.jazz-prod.id}.execute-api.${var.region}.amazonaws.com/prod\", >> stack_details.json"
   }  
 }
 
@@ -42,16 +42,16 @@ resource "null_resource" "outputVariablesBB" {
   count = "${var.scmbb}"
 
   provisioner "local-exec" {
-    command = "echo \"bitbucket_elb\" : \"http://${lookup(var.scmmap, "scm_elb")}\", >> stack_details.json"
+    command = "echo -e \"bitbucket_elb\" : \"http://${lookup(var.scmmap, "scm_elb")}\", >> stack_details.json"
   }
   provisioner "local-exec" {
-    command = "echo \"bitbucket_home\" : \"${lookup(var.scmmap, "scm_publicip")}\", >> stack_details.json"
+    command = "echo -e \"bitbucket_home\" : \"${lookup(var.scmmap, "scm_publicip")}\", >> stack_details.json"
   }
   provisioner "local-exec" {
-    command = "echo \"bitbucket_username\" : \"${lookup(var.scmmap, "scm_username")}\",  >> stack_details.json"
+    command = "echo -e \"bitbucket_username\" : \"${lookup(var.scmmap, "scm_username")}\",  >> stack_details.json"
   }
   provisioner "local-exec" {
-    command = "echo \"bitbucket_password\" : \"${lookup(var.scmmap, "scm_passwd")}\"  >> stack_details.json"
+    command = "echo -e \"bitbucket_password\" : \"${lookup(var.scmmap, "scm_passwd")}\"  >> stack_details.json"
   }
   provisioner "local-exec" {
     command = "echo } >> stack_details.json"
@@ -63,13 +63,13 @@ resource "null_resource" "outputVariablesGitlab" {
   count = "${var.scmgitlab}"
 
   provisioner "local-exec" {
-    command = "echo \"gitlab_home\" : \"http://${lookup(var.scmmap, "scm_publicip")}\", >> stack_details.json"
+    command = "echo -e \"gitlab_home\" : \"http://${lookup(var.scmmap, "scm_publicip")}\", >> stack_details.json"
   }
   provisioner "local-exec" {
-    command = "echo \"gitlab_username\" : \"${lookup(var.scmmap, "scm_username")}\",  >> stack_details.json"
+    command = "echo -e \"gitlab_username\" : \"${lookup(var.scmmap, "scm_username")}\",  >> stack_details.json"
   }
   provisioner "local-exec" {
-    command = "echo \"gitlab_password\" : \"${lookup(var.scmmap, "scm_passwd")}\"  >> stack_details.json"
+    command = "echo -e \"gitlab_password\" : \"${lookup(var.scmmap, "scm_passwd")}\"  >> stack_details.json"
   }
   provisioner "local-exec" {
     command = "echo } >> stack_details.json"
