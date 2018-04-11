b93552ba615a21dc7e4f81f01d503b1dad9aae57
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-04-11 14:30:23 -0400

    Port these fixes from another branch
    Former-commit-id: a1f872e6df60f687f4f6320b4decfea3d951c1e7

--- installscripts/cookbooks/git/recipes/default.rb
+++ installscripts/cookbooks/git/recipes/default.rb
@@ -1,13 +1,1 @@
-if node[:platform_family].include?("rhel")
-    execute 'gitinstall' do
-       command 'yum install git -y'
-    end
-  end
-  if node[:platform_family].include?("debian")
-     execute 'update-apt' do
-        command 'apt-get update'
-     end
-     execute 'gitinstall' do
-        command 'apt-get install git -y'
-     end
-  end
+package 'git'
