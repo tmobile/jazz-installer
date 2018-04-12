15c1a7108fc9ae8d1634540ea3734cd79e173e38
Surya Jakhotia <surya.jakhotia1@t-mobile.com>
2018-04-12 11:09:51 -0700

    Fix duplicate entries and update keys for stack details
    Former-commit-id: 0f0dfd6a1fd6c8b0bb0145430521739f569c24c6

--- installscripts/jazz-terraform-unix-noinstances/output.tf
+++ installscripts/jazz-terraform-unix-noinstances/output.tf
@@ -9,16 +9,14 @@ resource "null_resource" "outputVariables" {
   provisioner "local-exec" {
        command = <<EOF
                  echo { > stack_details.json
-                 echo \""jenkins_elb\"" : \""http://${lookup(var.jenkinsservermap, "jenkins_elb")}\"", >> stack_details.json
-                 echo \""jenkins_elb\"" : \""http://${lookup(var.jenkinsservermap, "jenkins_elb")}\"", >> stack_details.json
-                 echo \""jenkins_username\"" : \""${lookup(var.jenkinsservermap, "jenkinsuser")}\"",  >> stack_details.json
-                 echo \""jenkins_password\"" : \""${lookup(var.jenkinsservermap, "jenkinspasswd")}\"",  >> stack_details.json
-                 echo \""jenkins_subnet\"" : \""${lookup(var.jenkinsservermap, "jenkins_subnet")}\"",  >> stack_details.json
-                 echo \""jazz_home\"" : \""http://${aws_cloudfront_distribution.jazz.domain_name}\"",  >> stack_details.json
-                 echo \""jazz_admin_username\"" : \""${var.cognito_pool_username}\"",  >> stack_details.json
-                 echo \""jazz_admin_password\"" : \""${var.cognito_pool_password}\"",  >> stack_details.json
-                 echo \""region\"" : \""${var.region}\"",  >> stack_details.json
-                 echo \""api_endpoint\"" : \""https://${aws_api_gateway_rest_api.jazz-prod.id}.execute-api.${var.region}.amazonaws.com/prod\"",  >> stack_details.json
+                 echo \""Jenkins ELB\"" : \""http://${lookup(var.jenkinsservermap, "jenkins_elb")}\"", >> stack_details.json
+                 echo \""Jenkins Username\"" : \""${lookup(var.jenkinsservermap, "jenkinsuser")}\"",  >> stack_details.json
+                 echo \""Jenkins Password\"" : \""${lookup(var.jenkinsservermap, "jenkinspasswd")}\"",  >> stack_details.json
+                 echo \""Jazz Home\"" : \""http://${aws_cloudfront_distribution.jazz.domain_name}\"",  >> stack_details.json
+                 echo \""Jazz Admin Username\"" : \""${var.cognito_pool_username}\"",  >> stack_details.json
+                 echo \""Jazz Admin Password\"" : \""${var.cognito_pool_password}\"",  >> stack_details.json
+                 echo \""Region\"" : \""${var.region}\"",  >> stack_details.json
+                 echo \""Jazz API Endpoint\"" : \""https://${aws_api_gateway_rest_api.jazz-prod.id}.execute-api.${var.region}.amazonaws.com/prod\"",  >> stack_details.json
                  EOF
    }
 }
@@ -29,10 +27,9 @@ resource "null_resource" "outputVariablesBB" {
 
   provisioner "local-exec" {
     command = <<EOF
-              echo \""bitbucket_elb\"" : \""http://${lookup(var.scmmap, "scm_elb")}\"",  >> stack_details.json
-              echo \""bitbucket_home\"" : \""${lookup(var.scmmap, "scm_publicip")}\"",  >> stack_details.json
-              echo \""bitbucket_username\"" : \""${lookup(var.scmmap, "scm_username")}\"",   >> stack_details.json
-              echo \""bitbucket_password\"" : \""${lookup(var.scmmap, "scm_passwd")}\""  >> stack_details.json
+              echo \""Bitbucket ELB\"" : \""http://${lookup(var.scmmap, "scm_elb")}\"",  >> stack_details.json
+              echo \""Bitbucket Username\"" : \""${lookup(var.scmmap, "scm_username")}\"",   >> stack_details.json
+              echo \""Bitbucket Password\"" : \""${lookup(var.scmmap, "scm_passwd")}\""  >> stack_details.json
               echo } >> stack_details.json
               EOF
   }
@@ -44,9 +41,9 @@ resource "null_resource" "outputVariablesGitlab" {
 
   provisioner "local-exec" {
     command = <<EOF
-              echo \""gitlab_home\"" : \""http://${lookup(var.scmmap, "scm_publicip")}\"",  >> stack_details.json
-              echo \""gitlab_username\"" : \""${lookup(var.scmmap, "scm_username")}\"",   >> stack_details.json
-              echo \""gitlab_password\"" : \""${lookup(var.scmmap, "scm_passwd")}\""  >> stack_details.json
+              echo \""Gitlab Home\"" : \""http://${lookup(var.scmmap, "scm_publicip")}\"",  >> stack_details.json
+              echo \""Gitlab Username\"" : \""${lookup(var.scmmap, "scm_username")}\"",   >> stack_details.json
+              echo \""Gitlab Password\"" : \""${lookup(var.scmmap, "scm_passwd")}\""  >> stack_details.json
               echo } >> stack_details.json
               EOF
   }  
