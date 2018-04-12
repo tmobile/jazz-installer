0a0cc87c4d17bcbb493e11d3bb83d7100b297ab7
SiniWilson <Sini.Wilson@ust-global.com>
2018-04-12 11:39:42 +0530

    Fixing single quote Issue
    Former-commit-id: 351563ff0d853142fb6e45c1d887c508eff02853

--- installscripts/jazz-terraform-unix-noinstances/output.tf
+++ installscripts/jazz-terraform-unix-noinstances/output.tf
@@ -7,35 +7,20 @@ resource "null_resource" "outputVariables" {
     command = "touch stack_details.json"
   }
   provisioner "local-exec" {
-    command = "echo { > stack_details.json"  
-  }
-  provisioner "local-exec" {
-    command = "echo \"'jenkins_elb'\" : \"'http://${lookup(var.jenkinsservermap, "jenkins_elb")}'\", >> stack_details.json"
-  }
-  provisioner "local-exec" {
-    command = "echo \"'jenkins_username'\" : \"'${lookup(var.jenkinsservermap, "jenkinsuser")}'\",  >> stack_details.json"
-  }
-  provisioner "local-exec" {
-    command = "echo \"'jenkins_password'\" : \"'${lookup(var.jenkinsservermap, "jenkinspasswd")}'\",  >> stack_details.json"
-  } 
-  provisioner "local-exec" {
-    command = "echo \"'jenkins_subnet'\" : \"'${lookup(var.jenkinsservermap, "jenkins_subnet")}'\",  >> stack_details.json"
-  }
-  provisioner "local-exec" {
-    command = "echo \"'jazz_home'\" : \"'http://${aws_cloudfront_distribution.jazz.domain_name}'\",  >> stack_details.json"
-  }
-  provisioner "local-exec" {
-    command = "echo \"'jazz_admin_username'\" : \"'${var.cognito_pool_username}'\",  >> stack_details.json"
-  }
-  provisioner "local-exec" {
-    command = "echo \"'jazz_admin_password'\" : \"'${var.cognito_pool_password}'\",  >> stack_details.json"
-  }
-  provisioner "local-exec" {
-    command = "echo \"'region'\" : \"'${var.region}'\",  >> stack_details.json"
-  }
-  provisioner "local-exec" {
-    command = "echo \"'api_endpoint'\" : \"'https://${aws_api_gateway_rest_api.jazz-prod.id}.execute-api.${var.region}.amazonaws.com/prod'\",  >> stack_details.json"
-  }  
+       command = <<EOF
+                 echo { > stack_details.json
+                 echo \""jenkins_elb\"" : \""http://${lookup(var.jenkinsservermap, "jenkins_elb")}\"", >> stack_details.json
+                 echo \""jenkins_elb\"" : \""http://${lookup(var.jenkinsservermap, "jenkins_elb")}\"", >> stack_details.json
+                 echo \""jenkins_username\"" : \""${lookup(var.jenkinsservermap, "jenkinsuser")}\"",  >> stack_details.json
+                 echo \""jenkins_password\"" : \""${lookup(var.jenkinsservermap, "jenkinspasswd")}\"",  >> stack_details.json
+                 echo \""jenkins_subnet\"" : \""${lookup(var.jenkinsservermap, "jenkins_subnet")}\"",  >> stack_details.json
+                 echo \""jazz_home\"" : \""http://${aws_cloudfront_distribution.jazz.domain_name}\"",  >> stack_details.json
+                 echo \""jazz_admin_username\"" : \""${var.cognito_pool_username}\"",  >> stack_details.json
+                 echo \""jazz_admin_password\"" : \""${var.cognito_pool_password}\"",  >> stack_details.json
+                 echo \""region\"" : \""${var.region}\"",  >> stack_details.json
+                 echo \""api_endpoint\"" : \""https://${aws_api_gateway_rest_api.jazz-prod.id}.execute-api.${var.region}.amazonaws.com/prod\"",  >> stack_details.json
+                 EOF
+   }
 }
 
 resource "null_resource" "outputVariablesBB" {
@@ -43,36 +28,26 @@ resource "null_resource" "outputVariablesBB" {
   count = "${var.scmbb}"
 
   provisioner "local-exec" {
-    command = "echo \"'bitbucket_elb'\" : \"'http://${lookup(var.scmmap, "scm_elb")}'\",  >> stack_details.json"
+    command = <<EOF
+              echo \""bitbucket_elb\"" : \""http://${lookup(var.scmmap, "scm_elb")}\"",  >> stack_details.json
+              echo \""bitbucket_home\"" : \""${lookup(var.scmmap, "scm_publicip")}\"",  >> stack_details.json
+              echo \""bitbucket_username\"" : \""${lookup(var.scmmap, "scm_username")}\"",   >> stack_details.json
+              echo \""bitbucket_password\"" : \""${lookup(var.scmmap, "scm_passwd")}\""  >> stack_details.json
+              echo } >> stack_details.json
+              EOF
   }
-  provisioner "local-exec" {
-    command = "echo \"'bitbucket_home'\" : \"'${lookup(var.scmmap, "scm_publicip")}'\",  >> stack_details.json"
-  }
-  provisioner "local-exec" {
-    command = "echo \"'bitbucket_username'\" : \"'${lookup(var.scmmap, "scm_username")}'\",   >> stack_details.json"
-  }
-  provisioner "local-exec" {
-    command = "echo \"'bitbucket_password'\" : \"'${lookup(var.scmmap, "scm_passwd")}'\"  >> stack_details.json"
-  }
-  provisioner "local-exec" {
-    command = "echo } >> stack_details.json"
-  }
-}
+}  
 
 resource "null_resource" "outputVariablesGitlab" {
   depends_on = ["null_resource.outputVariables"]
   count = "${var.scmgitlab}"
 
   provisioner "local-exec" {
-    command = "echo \"'gitlab_home'\" : \"'http://${lookup(var.scmmap, "scm_publicip")}'\",  >> stack_details.json"
-  }
-  provisioner "local-exec" {
-    command = "echo \"'gitlab_username'\" : \"'${lookup(var.scmmap, "scm_username")}'\",   >> stack_details.json"
-  }
-  provisioner "local-exec" {
-    command = "echo \"'gitlab_password'\" : \"'${lookup(var.scmmap, "scm_passwd")}'\"  >> stack_details.json"
-  }
-  provisioner "local-exec" {
-    command = "echo } >> stack_details.json"
-  }
+    command = <<EOF
+              echo \""gitlab_home\"" : \""http://${lookup(var.scmmap, "scm_publicip")}\"",  >> stack_details.json
+              echo \""gitlab_username\"" : \""${lookup(var.scmmap, "scm_username")}\"",   >> stack_details.json
+              echo \""gitlab_password\"" : \""${lookup(var.scmmap, "scm_passwd")}\""  >> stack_details.json
+              echo } >> stack_details.json
+              EOF
+  }  
 }
