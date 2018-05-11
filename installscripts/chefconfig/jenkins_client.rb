1794ce4b622695a0694f0f2731364548c6791618
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-05-11 14:18:40 -0400

    Remove pointless centos reference

--- installscripts/chefconfig/jenkins_client.rb
+++ installscripts/chefconfig/jenkins_client.rb
@@ -1,4 +1,4 @@
 #root = File.absolute_path(File.dirname(__FILE__))
-# Scenario 1 expects cookbook location at /home/centos/cookbooks
+# Scenario 1 expects cookbook location at ~/cookbooks
 # Scenarios 2 & 3 expects cookbook location at /root/cookbooks
-cookbook_path [ '~/cookbooks', '/home/centos/cookbooks', '/root/cookbooks' ]
+cookbook_path [ '~/cookbooks', '/root/cookbooks' ]
