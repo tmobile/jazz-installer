df51933d4cc90313c6ec09b06f274252871072cc
SiniWilson <Sini.Wilson@ust-global.com>
2018-05-14 14:02:24 +0530

    Removing unwanted parameter from delete job
    Former-commit-id: 8bdcf1471c667c2158986567ccffc5f58048bb0c

--- installscripts/cookbooks/jenkins/files/jobs/job_build_pack_website.sh
+++ installscripts/cookbooks/jenkins/files/jobs/job_build_pack_website.sh
@@ -19,6 +19,7 @@ cat <<EOF | java -jar $JENKINS_CLI -s $JENKINS_URL -auth @$AUTHFILE create-job $
   <description></description>
   <keepDependencies>false</keepDependencies>
   <properties>
+  	<org.jenkinsci.plugins.workflow.job.properties.DisableConcurrentBuildsJobProperty/>
     <hudson.model.ParametersDefinitionProperty>
       <parameterDefinitions>
         <hudson.model.StringParameterDefinition>
