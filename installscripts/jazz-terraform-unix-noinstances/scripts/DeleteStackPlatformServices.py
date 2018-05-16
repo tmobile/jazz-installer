b7be145aa6b2e148963f55adcc4dc0f016299edb
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-05-16 12:53:12 -0400

    Fix bad merge
    Former-commit-id: d9479b15252802f96e55e0afa6d5034d47f8794b

--- installscripts/jazz-terraform-unix-noinstances/scripts/DeleteStackPlatformServices.py
+++ installscripts/jazz-terraform-unix-noinstances/scripts/DeleteStackPlatformServices.py
@@ -5,51 +5,78 @@ import subprocess
 import os
 import os.path
 
+
 def checkCFServiceAvailable(servicename):
-    return subprocess.call("aws cloudformation list-stack-resources --stack-name " + servicename, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    return subprocess.call(
+        "aws cloudformation list-stack-resources --stack-name " + servicename,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
+
 
 def deleteCFService(servicename):
-    return subprocess.call("aws cloudformation delete-stack --stack-name " + servicename, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    return subprocess.call(
+        "aws cloudformation delete-stack --stack-name " + servicename,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
+
+
 #return subprocess.call("aws cloudformation list-stack-resources --stack-name " + servicename, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
+
 def getServicesList():
-    return subprocess.call('aws cloudformation list-stacks --stack-status-filter "CREATE_IN_PROGRESS" "CREATE_FAILED" "CREATE_COMPLETE" "UPDATE_IN_PROGRESS" "UPDATE_COMPLETE" >> listservice.json', shell=True)
+    return subprocess.call(
+        'aws cloudformation list-stacks --stack-status-filter "CREATE_IN_PROGRESS" "CREATE_FAILED" "CREATE_COMPLETE" "UPDATE_IN_PROGRESS" "UPDATE_COMPLETE" >> listservice.json',
+        shell=True)
+
 
 def deleteCloudFormationService(service_name):
     return_code = checkCFServiceAvailable(service_name)
-    if return_code == 0 :
-        print ("Service::" + service_name + ": exists. Service deletion started.........")
+    if return_code == 0:
+        print("Service::" + service_name +
+              ": exists. Service deletion started.........")
         delete_return_code = deleteCFService(service_name)
-        if delete_return_code == 0 :
-            print ("\tSuccessfully Deleted service " + service_name)
-        else :
-            print ("\tError while Deleting service " + service_name + " errorcode=" + delete_return_code)
+        if delete_return_code == 0:
+            print("\tSuccessfully deleted service " + service_name)
+        else:
+            print("\tError while deleting service " + service_name +
+                  " errorcode=" + delete_return_code)
     else:
-        print ('Error Service not found::' + service_name)
+        print('Error service not found::' + service_name)
 
 
-if(len(sys.argv) != 3):
-    print ("Error - Please provide Argument1='stack name' and Argument1='Delete Client servies option true or false' ")
-    print ("Argument1=<StackName> Argument2=<true/false>")
+if (len(sys.argv) != 3):
+    print(
+        "Error - Please provide Argument1='stack name' and Argument1='Delete Client servies option true or false' "
+    )
+    print("Argument1=<StackName> Argument2=<true/false>")
     exit(1)
 
-print (str(sys.argv))
+print(str(sys.argv))
 
 stackName = sys.argv[1].lower() + "-"
 deleteClientServices = sys.argv[2]
 
-platformServices = ['jazz_cognito-authorizer', 'jazz_logs', 'jazz_usermanagement', 'jazz_services-handler', 'jazz_events', 'jazz_services', 'jazz_logout', 'jazz_login', 'jazz_cloud-logs-streamer', 'jazz_is-service-available', 'jazz_delete-serverless-service', 'jazz_create-serverless-service', 'jazz_email', 'jazz_events-handler', 'jazz_environments', 'jazz_scm-webhook' , 'jazz_environment-event-handler', 'jazz_deployments', 'jazz_deployments-event-handler']
+platformServices = [
+    'jazz_cognito-authorizer', 'jazz_logs', 'jazz_usermanagement',
+    'jazz_services-handler', 'jazz_events', 'jazz_services', 'jazz_logout',
+    'jazz_login', 'jazz_cloud-logs-streamer', 'jazz_is-service-available',
+    'jazz_delete-serverless-service', 'jazz_create-serverless-service',
+    'jazz_email', 'jazz_events-handler', 'jazz_environments',
+    'jazz_scm-webhook', 'jazz_environment-event-handler', 'jazz_deployments',
+    'jazz_deployments-event-handler'
+]
 
 for pservice in platformServices:
-    deleteCloudFormationService(stackName + pservice)
+    deleteCloudFormationService(stackName + pservice + '-prod')
 
-print ("\r\n\r\nCompleted Deletion Platform services.")
+print("\r\n\r\nCompleted deletion of platform services.")
 
-if (deleteClientServices.lower() != 'true'):
 if (deleteClientServices.lower() != 'true'):
     exit(0)
 
-print ("\r\nStarting deletions of Client Services\r\n\r\n")
+print("\r\nStarting deletion of client services\r\n\r\n")
 
 ## Delete user services Cloud formations
 fname = 'listservice.json'
@@ -65,12 +92,12 @@ jsonFile.close()
 
 ss = values['StackSummaries']
 
-
 spLen = len(stackName)
 
-inval = 0;
+inval = 0
 for item in ss:
     if (item['StackName'].startswith(stackName)):
         #print(item['StackName'][spLen:len(item['StackName'])])
-        if item['StackName'][spLen:len(item['StackName'])] not in platformServices:
+        if item['StackName'][spLen:len(item[
+                'StackName'])] not in platformServices:
             deleteCloudFormationService(item['StackName'])
