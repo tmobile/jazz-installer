8ad7b54852d8f86420f5aa5783904936ff2ba7cb
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-05-17 12:53:09 -0400

    Try to make cookbook locations consistent for all
    Former-commit-id: b9d4a411c3ea50667e50c545b79d59e659847018

--- installscripts/chefconfig/jenkins_client.rb
+++
@@ -1,4 +0,0 @@
-#root = File.absolute_path(File.dirname(__FILE__))
-# Scenario 1 expects cookbook location at ~/cookbooks
-# Scenarios 2 & 3 expects cookbook location at /root/cookbooks
-cookbook_path [ '~/cookbooks', '/root/cookbooks' ]
