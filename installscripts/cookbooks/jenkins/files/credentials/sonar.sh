c1da6205077d863132bd3f46aa6631dde1bede4a
Rajeev <rajeev.chettiyar@ust-global.com>
2018-05-24 11:36:09 +0530

    Installer var json update with jenkins credentials for sonar
    Former-commit-id: c8802c5907e19a562005a7abc95991b8b119e6f7

---
+++ installscripts/cookbooks/jenkins/files/credentials/sonar.sh
@@ -0,0 +1,21 @@
+JENKINS_URL=http://$1/ # localhost or jenkins elb url
+SSH_USER=$2
+
+if [ -f /etc/redhat-release ]; then
+  AUTHFILE=/home/$SSH_USER/cookbooks/jenkins/files/default/authfile
+  JENKINS_CLI=/home/$SSH_USER/jenkins-cli.jar
+elif [ -f /etc/lsb-release ]; then
+  AUTHFILE=/root/cookbooks/jenkins/files/default/authfile
+  JENKINS_CLI=/root/jenkins-cli.jar
+fi
+
+echo "$0 $1 $2 "
+cat <<EOF | java -jar $JENKINS_CLI -s $JENKINS_URL -auth @$AUTHFILE create-credentials-by-xml system::system::jenkins "(global)"
+<com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl>
+  <scope>GLOBAL</scope>
+  <id>ADMIN_SONAR_CREDENTIAL</id>
+  <description>SONAR CREDENTIALS</description>
+  <username>sonaruser</username>
+  <password>sonarpasswd</password>
+</com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl>
+EOF
