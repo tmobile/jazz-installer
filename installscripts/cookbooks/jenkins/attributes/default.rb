d4dd21721b319034880f9c16b306e44c0f65cc00
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-05-25 17:13:48 -0400

    Add explicit node version

--- installscripts/cookbooks/jenkins/attributes/default.rb
+++ installscripts/cookbooks/jenkins/attributes/default.rb
@@ -23,3 +23,6 @@ default['bbpassword'] = 'REPLACEME'
 #Maven cookbook property
 default['maven']['version'] = "3.5.2"
 default['maven']['setup_bin'] = true
+default['maven']['setup_bin'] = true
+#Node cookbook property
+default['nodejs']['version'] = "8.11.2"
