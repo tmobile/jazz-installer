e0bf3d727e174cecc58484cf5fff1ca59934d40a
rajeevr2715 <36688599+rajeevr2715@users.noreply.github.com>
2018-07-25 15:46:45 +0530

    Enable tagging of AWS resources (#290)
    * Parse and validate the Tags
    
    * Inject additional tags into terraform vars and installer vars json files
    
    * Modified Terrafrom aws resources tag definition
    
    * Rreview changes - Moved installer vars write and Tags with locals
    
    * Unit Test - Validate tag
    
    * Rreview changes - rename aws tag installer vars json
    
    
    Former-commit-id: 730691072f58434e35b3ad61ab932c3ca3045c28

--- installscripts/jazz-terraform-unix-noinstances/scripts/modifyPropertyFile.sh
+++ installscripts/jazz-terraform-unix-noinstances/scripts/modifyPropertyFile.sh
@@ -5,5 +5,9 @@ property_value=$2
 property_file_json=$3
 
 property_value=$(sed 's/[]\/$*.^|[]/\\&/g' <<< $property_value)
-
-sed -i "s/\"$property_key\".*\"\"/\"$property_key\" : \"$property_value\"/g" $property_file_json
+if [ "$4" == "nostring" ]; then
+  property_value=$(sed "s/'/\"/g" <<< "$property_value")
+  sed -i "s/$property_key\".*.$/$property_key\" : $property_value/g" $property_file_json
+else
+  sed -i "s/\"$property_key\".*\"\"/\"$property_key\" : \"$property_value\"/g" $property_file_json
+fi
