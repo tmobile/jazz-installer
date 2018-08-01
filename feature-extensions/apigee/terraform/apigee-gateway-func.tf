e6487993288d3244b4a202e485b25ece89071ac9
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-08-01 16:40:57 -0400

    Zip in Terraform
    Former-commit-id: a0949e4b9060a32bfd4de6e24792306d36098a0a

--- feature-extensions/apigee/terraform/apigee-gateway-func.tf
+++ feature-extensions/apigee/terraform/apigee-gateway-func.tf
@@ -1,3 +1,10 @@
+data "archive_file" "apigee-gateway-zip" {
+  type        = "zip"
+  source_dir = "${path.module}/../jazz-apigee-proxy"
+  output_path = "${path.module}/../jazz-apigee-proxy.zip"
+}
+
+
 resource "aws_iam_role" "iam_for_apigee_lambda" {
   name = "iam_for_apigee_lambda"
 
@@ -7,8 +14,7 @@ resource "aws_iam_role" "iam_for_apigee_lambda" {
   "Statement": [
     {
       "Action": [
-        "sts:AssumeRole",
-        "lambda:invokeFunction"
+        "sts:AssumeRole"
       ],
       "Principal": {
         "Service": "lambda.amazonaws.com"
@@ -23,11 +29,11 @@ EOF
 
 #TODO figure out how to build zip.
 resource "aws_lambda_function" "jazz-apigee-proxy" {
-  filename         = "../jazz-apigee-proxy.zip"
+  filename         = "${data.archive_file.apigee-gateway-zip.output_path}"
   function_name    = "jazz-apigee-proxy"
   role             = "${aws_iam_role.iam_for_apigee_lambda.arn}"
   handler          = "jazz-apigee-proxy.handler"
-  source_code_hash = "${base64sha256(file("../jazz-apigee-proxy.zip"))}"
+  source_code_hash = "${data.archive_file.apigee-gateway-zip.output_base64sha256}"
   runtime          = "nodejs8.10"
 
   # environment {
