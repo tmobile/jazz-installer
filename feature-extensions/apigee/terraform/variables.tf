42a3feb12725eed98bfcdfa599890fcd520eea50
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-08-09 16:19:03 -0400

    Revert "Merge branch 'v1.8.1' into apigee-installer"
    This reverts commit 86d620faf1a66c31a6d0182c61338ab91b2dba8d [formerly 778d1a48521acfa696fef1b26a7f532f7cb5d313], reversing
    changes made to 550fd03a3ff2e1dd346da66be6eb0fe7d2f9881b [formerly 7cbd0d78d3c6cede5d041c7a7a68a37d654461a4].
    
    
    Former-commit-id: af08988119ae49ed3f02c1b1156b24375975afd8

--- feature-extensions/apigee/terraform/variables.tf
+++
@@ -1,4 +0,0 @@
-# These variables are set by CLI args from the calling script
-variable "region" { type = "string" default = "us-east-1" }
-variable "jazz_aws_accountid" {type = "string"}
-variable "env_prefix" {type = "string"}
