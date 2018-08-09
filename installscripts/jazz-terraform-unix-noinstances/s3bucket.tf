e4d2496666f3c377e2def63cd5d82f785bef7635
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-08-09 16:50:35 -0400

    Move stuff that has nothing to do with s3 buckets out of s3 script
    Former-commit-id: 113fd723bd26c5289a1c5b2d09b853e8105808cc

--- installscripts/jazz-terraform-unix-noinstances/s3bucket.tf
+++ installscripts/jazz-terraform-unix-noinstances/s3bucket.tf
@@ -55,8 +55,8 @@ resource "aws_s3_bucket" "oab-apis-deployment-prod" {
 resource "aws_s3_bucket" "jazz_s3_api_doc" {
   bucket_prefix = "${var.envPrefix}-jazz-s3-api-doc-"
   request_payer = "BucketOwner"
-  //region = "${var.region}"
-  depends_on = ["aws_api_gateway_rest_api.jazz-prod" ]
+  #TODO Verify this is a spurious depend.
+  # depends_on = ["aws_api_gateway_rest_api.jazz-prod" ]
   acl = "public-read"
   cors_rule {
     allowed_headers = ["Authorization"]
@@ -69,32 +69,6 @@ resource "aws_s3_bucket" "jazz_s3_api_doc" {
 
   website {
     index_document = "index.html"
-  }
-}
-
-resource "aws_api_gateway_rest_api" "jazz-dev" {
-  name        = "${var.envPrefix}-dev"
-  description = "DEV API gateway"
-}
-resource "aws_api_gateway_rest_api" "jazz-stag" {
-  name        = "${var.envPrefix}-stag"
-  description = "STG API"
-}
-
-resource "aws_api_gateway_rest_api" "jazz-prod" {
-  name        = "${var.envPrefix}-prod"
-  description = "PROD API"
-
-  #TODO None of these steps have ANYTHING to do with S3
-  provisioner "local-exec" {
-    command = "rm -rf jazz-core"
-  }
-  provisioner "local-exec" {
-    command = "git clone -b ${var.github_branch} ${var.github_repo} jazz-core --depth 1"
-
-  }
-  provisioner "local-exec" {
-    command = "${var.configureApikey_cmd} ${aws_api_gateway_rest_api.jazz-dev.id} ${aws_api_gateway_rest_api.jazz-stag.id} ${aws_api_gateway_rest_api.jazz-prod.id} ${var.jenkinsjsonpropsfile} ${var.jenkinsattribsfile} ${var.envPrefix}"
   }
 }
 
@@ -153,119 +127,6 @@ resource "aws_iam_policy" "basic_execution_policy" {
 EOF
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name = "${var.envPrefix}_lambda2_basic_execution_1"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-        {
-            "Sid": "",
-            "Effect": "Allow",
-            "Principal": {
-                        "Service": "apigateway.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        },
-        {
-            "Effect": "Allow",
-            "Principal": {
-                        "Service": "lambda.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }
-   ]
-}
-EOF
-
-  provisioner "local-exec" {
-    when = "destroy"
-    on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/AmazonAPIGatewayInvokeFullAccess"
-  }
-  provisioner "local-exec" {
-    when = "destroy"
-    on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn ${aws_iam_policy.basic_execution_policy.arn}"
-  }
-  provisioner "local-exec" {
-    when = "destroy"
-    on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/AWSLambdaFullAccess"
-  }
-  provisioner "local-exec" {
-    when = "destroy"
-    on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/AmazonKinesisFullAccess"
-  }
-  provisioner "local-exec" {
-    when = "destroy"
-    on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/job-function/NetworkAdministrator"
-  }
-  provisioner "local-exec" {
-    when = "destroy"
-    on_failure = "continue"
-    when = "destroy"
-    on_failure = "continue"
-    when = "destroy"
-    on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/AmazonVPCCrossAccountNetworkInterfaceOperations"
-  }
-  provisioner "local-exec" {
-    when = "destroy"
-    on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess"
-  }
-  provisioner "local-exec" {
-    when = "destroy"
-    on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/AmazonCognitoPowerUser"
-  }
-  provisioner "local-exec" {
-    when = "destroy"
-    on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-  }
-}
-
-
-resource "aws_iam_role_policy_attachment" "lambdafullaccess" {
-  role       = "${aws_iam_role.lambda_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/AWSLambdaFullAccess"
-}
-resource "aws_iam_role_policy_attachment" "apigatewayinvokefullAccess" {
-  role       = "${aws_iam_role.lambda_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonAPIGatewayInvokeFullAccess"
-}
-resource "aws_iam_role_policy_attachment" "cloudwatchlogaccess" {
-  role       = "${aws_iam_role.lambda_role.name}"
-  policy_arn = "${aws_iam_policy.basic_execution_policy.arn}"
-}
-resource "aws_iam_role_policy_attachment" "kinesisaccess" {
-  role       = "${aws_iam_role.lambda_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonKinesisFullAccess"
-}
-resource "aws_iam_role_policy_attachment" "networkadminaccess" {
-  role       = "${aws_iam_role.lambda_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/job-function/NetworkAdministrator"
-}
-resource "aws_iam_role_policy_attachment" "vpccrossaccountaccess" {
-  role       = "${aws_iam_role.lambda_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCCrossAccountNetworkInterfaceOperations"
-}
-resource "aws_iam_role_policy_attachment" "s3fullaccess" {
-  role       = "${aws_iam_role.lambda_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-resource "aws_iam_role_policy_attachment" "cognitopoweruser" {
-  role       = "${aws_iam_role.lambda_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonCognitoPowerUser"
-}
-resource "aws_iam_role_policy_attachment" "vpcaccessexecution" {
-  role       = "${aws_iam_role.lambda_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-}
 resource "aws_s3_bucket" "dev-serverless-static" {
   bucket_prefix = "${var.envPrefix}-dev-web-"
   request_payer = "BucketOwner"
