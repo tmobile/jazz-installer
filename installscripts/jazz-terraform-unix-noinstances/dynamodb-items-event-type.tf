60ae73dd618951eed398342d4d52daaa4eb4438a
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-08-08 20:17:23 -0400

    Do the rest of the dynamodb items
    Former-commit-id: 9db2c7b8063c096b5917869d081014305ef59c26

---
+++ installscripts/jazz-terraform-unix-noinstances/dynamodb-items-event-type.tf
@@ -0,0 +1,42 @@
+locals {
+  event_type_names = [
+    "SERVICE_UPDATE",
+    "SERVICE_DEPLOYMENT",
+    "SERVICE_CREATION",
+    "SERVICE_DELETION",
+    "SERVICE_ONBOARDING"
+  ]
+}
+
+resource "aws_dynamodb_table_item" "event-type-dev" {
+  count = "${length(local.event_type_names)}"
+  table_name = "${aws_dynamodb_table.dynamodb-table_Event_Type_Dev.name}"
+  hash_key = "${aws_dynamodb_table.dynamodb-table_Event_Type_Dev.hash_key}"
+  item = <<ITEM
+{
+  "EVENT_TYPE": {"S": "${element(local.event_type_names, count.index)}"}
+}
+ITEM
+}
+
+resource "aws_dynamodb_table_item" "event-type-stg" {
+  count = "${length(local.event_type_names)}"
+  table_name = "${aws_dynamodb_table.dynamodb-table_Event_Type_Stg.name}"
+  hash_key = "${aws_dynamodb_table.dynamodb-table_Event_Type_Stg.hash_key}"
+  item = <<ITEM
+{
+  "EVENT_TYPE": {"S": "${element(local.event_type_names, count.index)}"}
+}
+ITEM
+}
+
+resource "aws_dynamodb_table_item" "event-type-prod" {
+  count = "${length(local.event_type_names)}"
+  table_name = "${aws_dynamodb_table.dynamodb-table_Event_Type_Prod.name}"
+  hash_key = "${aws_dynamodb_table.dynamodb-table_Event_Type_Prod.hash_key}"
+  item = <<ITEM
+{
+  "EVENT_TYPE": {"S": "${element(local.event_type_names, count.index)}"}
+}
+ITEM
+}
