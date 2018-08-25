7fbff7e083a1ded844799b8b332a720b9762650a
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-08-24 21:10:32 -0400

    Try this basic Travis lint check

---
+++ test/check-tf.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# This is inspired by:
+# https://github.com/mozilla-platform-ops/devservices-aws/blob/master/runtests.sh
+set -euo pipefail
+main() {
+  echo -e '\n-----> Running terraform validate'
+  for d in $(git ls-files '*.tf' | xargs -n1 dirname | LC_ALL=C sort | uniq); do
+    echo -en "${d} "
+    terraform validate "${d}"
+    echo "✓"
+  done
+  echo -e '\n-----> Success!'
+}
+main "$@"
