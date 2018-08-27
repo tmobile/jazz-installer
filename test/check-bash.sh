c8e548b5dafebc2ee7dc416d864b14e3930a22e8
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-08-27 18:51:48 -0400

    Try this

--- test/check-bash.sh
+++ test/check-bash.sh
@@ -6,11 +6,6 @@ else
   BRANCH="master"
 fi
 
-exitcode=0
 for file in $(git diff --name-only $BRANCH | grep .sh\$); do
-  if [[ "$(shellcheck "$file")" -gt 0 ]]; then
-    echo "ERROR: $file failed to pass shellcheck lint step"
-    exitcode=1
-  fi
+  shellcheck "$file"
 done
-exit $exitcode
