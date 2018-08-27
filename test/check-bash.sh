fdc48d2c0fc35709306eec19bf6a3e636900d6c1
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-08-27 18:20:47 -0400

    Add `shellcheck` to catch shell script syntax errors

--- test/check-bash.sh
+++ test/check-bash.sh
@@ -3,10 +3,12 @@
 # https://github.com/mozilla-platform-ops/devservices-aws/blob/master/runtests.sh
 set -euo pipefail
 main() {
-  echo -e '\n-----> Running terraform validate'
-  cd $TRAVIS_BUILD_DIR/installscripts/jazz-terraform-unix-noinstances
-  terraform init
-  terraform validate
+  echo -e '\n-----> Running shellcheck'
+  for d in $(git ls-files '*.sh' | xargs -n1 dirname | LC_ALL=C sort | uniq); do
+    echo -en "${d} "
+    shellcheck "${d}"
+    echo "✓"
+  done
   echo -e '\n-----> Success!'
 }
 main "$@"
