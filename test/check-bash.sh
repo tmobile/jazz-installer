66cf026efb24c41549b50d2fe4ad799b81d84bc9
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-09-06 15:38:39 -0400

    Ignore shellcheck SC2024 and fix linting errors in centos script

--- test/check-bash.sh
+++ test/check-bash.sh
@@ -7,5 +7,7 @@ else
 fi
 
 for file in $(git diff --name-only $BRANCH | grep .sh\$); do
-  shellcheck "$file"
+  # Globally ignore lint error SC2024: https://github.com/koalaman/shellcheck/wiki/SC2024
+  # as I don't think it's an important check for our use case
+  shellcheck -e SC2024 "$file"
 done
