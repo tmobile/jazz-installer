388c105c15ef8d0dce695a37f2c7cf5e2fe395ab
devsatishm <dev.satishm@gmail.com>
2018-09-07 02:13:52 -0700

    Removed unwanted additional IAM policies

--- installscripts/jazz-terraform-unix-noinstances/s3bucket.tf
+++ installscripts/jazz-terraform-unix-noinstances/s3bucket.tf
@@ -3,7 +3,6 @@ data "aws_canonical_user_id" "current" {}
 resource "aws_s3_bucket" "oab-apis-deployment-dev" {
   bucket_prefix = "${var.envPrefix}-apis-deployment-dev-"
   request_payer = "BucketOwner"
-  //region = "${var.region}"
   cors_rule {
     allowed_headers = ["Authorization"]
     allowed_methods = ["GET"]
@@ -18,17 +17,16 @@ resource "aws_s3_bucket" "oab-apis-deployment-dev" {
   provisioner "local-exec" {
     command = "${var.sets3acl_cmd} ${aws_s3_bucket.oab-apis-deployment-dev.bucket} ${data.aws_canonical_user_id.current.id}"
   }
+
   provisioner "local-exec" {
     when = "destroy"
     on_failure = "continue"
     command = "	aws s3 rm s3://${aws_s3_bucket.oab-apis-deployment-dev.bucket} --recursive"
   }
-
 }
 resource "aws_s3_bucket" "oab-apis-deployment-stg" {
   bucket_prefix = "${var.envPrefix}-apis-deployment-stg-"
   request_payer = "BucketOwner"
-  //region = "${var.region}"
   cors_rule {
     allowed_headers = ["Authorization"]
     allowed_methods = ["GET"]
@@ -47,12 +45,10 @@ resource "aws_s3_bucket" "oab-apis-deployment-stg" {
     on_failure = "continue"
     command = "	aws s3 rm s3://${aws_s3_bucket.oab-apis-deployment-stg.bucket} --recursive"
   }
-
 }
 resource "aws_s3_bucket" "oab-apis-deployment-prod" {
   bucket_prefix = "${var.envPrefix}-apis-deployment-prod-"
   request_payer = "BucketOwner"
-  //region = "${var.region}"
   cors_rule {
     allowed_headers = ["Authorization"]
     allowed_methods = ["GET"]
@@ -71,13 +67,11 @@ resource "aws_s3_bucket" "oab-apis-deployment-prod" {
     on_failure = "continue"
     command = "	aws s3 rm s3://${aws_s3_bucket.oab-apis-deployment-prod.bucket} --recursive"
   }
-
 }
 
 resource "aws_s3_bucket" "jazz_s3_api_doc" {
   bucket_prefix = "${var.envPrefix}-jazz-s3-api-doc-"
   request_payer = "BucketOwner"
-  //region = "${var.region}"
   depends_on = ["aws_api_gateway_rest_api.jazz-prod" ]
   acl = "public-read"
   cors_rule {
@@ -102,16 +96,16 @@ resource "aws_s3_bucket" "jazz_s3_api_doc" {
 
 resource "aws_api_gateway_rest_api" "jazz-dev" {
   name        = "${var.envPrefix}-dev"
-  description = "DEV API gateway"
+  description = "DEV API Gateway"
 }
-resource "aws_api_gateway_rest_api" "jazz-stag" {
-  name        = "${var.envPrefix}-stag"
-  description = "STG API"
+resource "aws_api_gateway_rest_api" "jazz-stg" {
+  name        = "${var.envPrefix}-stg"
+  description = "STG API Gateway"
 }
 
 resource "aws_api_gateway_rest_api" "jazz-prod" {
   name        = "${var.envPrefix}-prod"
-  description = "PROD API"
+  description = "PROD API Gateway"
 
   provisioner "local-exec" {
     command = "rm -rf jazz-core"
@@ -121,14 +115,13 @@ resource "aws_api_gateway_rest_api" "jazz-prod" {
 
   }
   provisioner "local-exec" {
-    command = "${var.configureApikey_cmd} ${aws_api_gateway_rest_api.jazz-dev.id} ${aws_api_gateway_rest_api.jazz-stag.id} ${aws_api_gateway_rest_api.jazz-prod.id} ${var.region} ${var.jenkinsjsonpropsfile} ${var.jenkinsattribsfile} ${var.envPrefix}"
+    command = "${var.configureApikey_cmd} ${aws_api_gateway_rest_api.jazz-dev.id} ${aws_api_gateway_rest_api.jazz-stg.id} ${aws_api_gateway_rest_api.jazz-prod.id} ${var.region} ${var.jenkinsjsonpropsfile} ${var.jenkinsattribsfile} ${var.envPrefix}"
   }
 }
 
 resource "aws_s3_bucket" "jazz-web" {
   bucket_prefix = "${var.envPrefix}-web-"
   request_payer = "BucketOwner"
-  //region = "${var.region}"
   depends_on = ["aws_s3_bucket.jazz_s3_api_doc" ]
   cors_rule {
     allowed_headers = ["Authorization"]
@@ -142,7 +135,6 @@ resource "aws_s3_bucket" "jazz-web" {
   }
   website {
     index_document = "index.html"
-
     routing_rules = <<EOF
 [{
     "Condition": {
@@ -162,107 +154,67 @@ EOF
   provisioner "local-exec" {
     when = "destroy"
     on_failure = "continue"
-    command = "	aws s3 rm s3://${aws_s3_bucket.jazz-web.bucket}/ --recursive"
+    command = "aws s3 rm s3://${aws_s3_bucket.jazz-web.bucket}/ --recursive"
   }
 }
 
-resource "aws_iam_policy" "basic_execution_policy" {
-  name        = "${var.envPrefix}_execution_aws_logs"
-  path        = "/"
-  description = "aws_logs access policy"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:*"
-            ],
-            "Resource": "arn:aws:logs:*:*:*"
-        }
-    ]
-}
-EOF
-}
-
 resource "aws_iam_role" "lambda_role" {
-  name = "${var.envPrefix}_lambda2_basic_execution_1"
+  name = "${var.envPrefix}_basic_execution"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
-        {
-            "Sid": "",
-            "Effect": "Allow",
-            "Principal": {
-                        "Service": "apigateway.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
+    {
+        "Sid": "",
+        "Effect": "Allow",
+        "Principal": {
+            "Service": "apigateway.amazonaws.com"
         },
-        {
-            "Effect": "Allow",
-            "Principal": {
-                        "Service": "lambda.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }
-   ]
+        "Action": "sts:AssumeRole"
+    },
+    {
+        "Effect": "Allow",
+        "Principal": {
+            "Service": "lambda.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+    }
+  ]
 }
 EOF
 
   provisioner "local-exec" {
     when = "destroy"
     on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/AmazonAPIGatewayInvokeFullAccess"
+    command = "aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/AmazonAPIGatewayInvokeFullAccess"
   }
   provisioner "local-exec" {
     when = "destroy"
     on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn ${aws_iam_policy.basic_execution_policy.arn}"
+    command = "aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   }
   provisioner "local-exec" {
     when = "destroy"
     on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/AWSLambdaFullAccess"
+    command = "aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/AWSLambdaFullAccess"
   }
   provisioner "local-exec" {
     when = "destroy"
     on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/AmazonKinesisFullAccess"
+    command = "aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/AmazonKinesisFullAccess"
+  }
+
+  provisioner "local-exec" {
+    when = "destroy"
+    on_failure = "continue"
+    command = "aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess"
   }
   provisioner "local-exec" {
     when = "destroy"
     on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/job-function/NetworkAdministrator"
-  }
-  provisioner "local-exec" {
-    when = "destroy"
-    on_failure = "continue"
-    when = "destroy"
-    on_failure = "continue"
-    when = "destroy"
-    on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/AmazonVPCCrossAccountNetworkInterfaceOperations"
-  }
-  provisioner "local-exec" {
-    when = "destroy"
-    on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess"
-  }
-  provisioner "local-exec" {
-    when = "destroy"
-    on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/AmazonCognitoPowerUser"
-  }
-  provisioner "local-exec" {
-    when = "destroy"
-    on_failure = "continue"
-    command = " aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+    command = "aws iam detach-role-policy --role-name ${aws_iam_role.lambda_role.name} --policy-arn arn:aws:iam::aws:policy/AmazonCognitoPowerUser"
   }
 }
-
 
 resource "aws_iam_role_policy_attachment" "lambdafullaccess" {
   role       = "${aws_iam_role.lambda_role.name}"
@@ -274,19 +226,11 @@ resource "aws_iam_role_policy_attachment" "apigatewayinvokefullAccess" {
 }
 resource "aws_iam_role_policy_attachment" "cloudwatchlogaccess" {
   role       = "${aws_iam_role.lambda_role.name}"
-  policy_arn = "${aws_iam_policy.basic_execution_policy.arn}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 resource "aws_iam_role_policy_attachment" "kinesisaccess" {
   role       = "${aws_iam_role.lambda_role.name}"
   policy_arn = "arn:aws:iam::aws:policy/AmazonKinesisFullAccess"
-}
-resource "aws_iam_role_policy_attachment" "networkadminaccess" {
-  role       = "${aws_iam_role.lambda_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/job-function/NetworkAdministrator"
-}
-resource "aws_iam_role_policy_attachment" "vpccrossaccountaccess" {
-  role       = "${aws_iam_role.lambda_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCCrossAccountNetworkInterfaceOperations"
 }
 resource "aws_iam_role_policy_attachment" "s3fullaccess" {
   role       = "${aws_iam_role.lambda_role.name}"
@@ -296,14 +240,10 @@ resource "aws_iam_role_policy_attachment" "cognitopoweruser" {
   role       = "${aws_iam_role.lambda_role.name}"
   policy_arn = "arn:aws:iam::aws:policy/AmazonCognitoPowerUser"
 }
-resource "aws_iam_role_policy_attachment" "vpcaccessexecution" {
-  role       = "${aws_iam_role.lambda_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-}
 resource "aws_s3_bucket" "dev-serverless-static" {
   bucket_prefix = "${var.envPrefix}-dev-web-"
   request_payer = "BucketOwner"
-  //region = "${var.region}"
+
   cors_rule {
     allowed_headers = ["Authorization"]
     allowed_methods = ["GET"]
@@ -319,13 +259,12 @@ resource "aws_s3_bucket" "dev-serverless-static" {
     on_failure = "continue"
     command = "	aws s3 rm s3://${aws_s3_bucket.dev-serverless-static.bucket} --recursive"
   }
-
 }
 
 resource "aws_s3_bucket" "stg-serverless-static" {
   bucket_prefix = "${var.envPrefix}-stg-web-"
   request_payer = "BucketOwner"
-  //region = "${var.region}"
+
   cors_rule {
     allowed_headers = ["Authorization"]
     allowed_methods = ["GET"]
@@ -347,7 +286,7 @@ resource "aws_s3_bucket" "stg-serverless-static" {
 resource "aws_s3_bucket" "prod-serverless-static" {
   bucket_prefix = "${var.envPrefix}-prod-web-"
   request_payer = "BucketOwner"
-  //region = "${var.region}"
+
   cors_rule {
     allowed_headers = ["Authorization"]
     allowed_methods = ["GET"]
@@ -451,7 +390,6 @@ data "aws_iam_policy_document" "prod-serverless-static-policy-data-contents" {
     resources = [
       "${aws_s3_bucket.prod-serverless-static.arn}/*"
     ]
-
   }
   statement {
     sid = "ListBucket"
@@ -466,7 +404,6 @@ data "aws_iam_policy_document" "prod-serverless-static-policy-data-contents" {
       "${aws_s3_bucket.prod-serverless-static.arn}"
     ]
   }
-
 }
 resource "aws_s3_bucket_policy" "prod-serverless-static-bucket-contents-policy" {
   bucket = "${aws_s3_bucket.prod-serverless-static.id}"
