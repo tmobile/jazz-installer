29d7d0bfe579ac651f4503e446f4792a3336b744
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-09-07 12:35:16 -0400

    :D
    Former-commit-id: 3af0b45bcd28ef7db9960a30cb9cf0c335e6f913

--- test/check-py.sh
+++ test/check-py.sh
@@ -7,7 +7,7 @@ normal=$(tput sgr0)
 
 echo "Diffing commit range" "$TRAVIS_COMMIT_RANGE"
 
-for file in $(git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep .sh\$); do
+for file in $(git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep .py\$); do
   echo "Checking ${bold}$file${normal}..."
   flake8 "$file"
 done
