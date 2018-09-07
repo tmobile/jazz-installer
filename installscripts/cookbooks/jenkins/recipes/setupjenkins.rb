e4a17974e06764d1a9ffa6a1ebed21edf96df07d
Siddharth Vidhani <svidhani@yahoo.com>
2018-09-07 15:10:48 -0700

    Update setupjenkins.rb
    Former-commit-id: 12fdeb98388182efacc3d40f1fc3a7eaf7d2bf23

--- installscripts/cookbooks/jenkins/recipes/setupjenkins.rb
+++ installscripts/cookbooks/jenkins/recipes/setupjenkins.rb
@@ -7,7 +7,7 @@ if node['dockerizedJenkins'] == false
   include_recipe 'cloudcli'
 
   npm_package 'serverless' do
-    version '1.28.0'
+    version '1.30.0'
   end
 
   npm_package '@angular/cli' do
