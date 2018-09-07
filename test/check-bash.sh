856620a3bd0426e74c880423af00752aa28b092d
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-09-07 11:59:46 -0400

    These linter scripts should be more robust

--- test/check-bash.sh
+++ test/check-bash.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-if [[ -z "$TRAVIS_PULL_REQUEST_BRANCH" ]]; then
-  BRANCH="$TRAVIS_PULL_REQUEST_BRANCH"
+set -eo pipefail
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+if [[ -z "$TRAVIS_BRANCH" ]]; then
+  BRANCH="$TRAVIS_BRANCH"
 else
   BRANCH="master"
 fi
 
-echo "Diffing with " $BRANCH "and TRAVIS_BRANCH is " "$TRAVIS_BRANCH"
+echo "Diffing with" "$BRANCH"
 
-for file in $(git diff --name-only $BRANCH | grep .sh\$); do
+for file in $(git diff --name-only "$TRAVIS_BRANCH" | grep .sh\$); do
   # Globally ignore lint error SC2024: https://github.com/koalaman/shellcheck/wiki/SC2024
   # as I don't think it's an important check for our use case
-  shellcheck -e SC2024 "$file"
+  echo "Checking ${bold}$file${normal}..."
+  shellcheck -e SC2024 $file
 done
