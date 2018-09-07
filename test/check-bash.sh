e524a2d640cef09859bd1537154c1e9974616afd
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-09-07 12:25:59 -0400

    Derp
    Former-commit-id: b81cbd4ca412e447c0968603c1f44c8405f1bc70

--- test/check-bash.sh
+++ test/check-bash.sh
@@ -11,9 +11,9 @@ normal=$(tput sgr0)
 #   BRANCH="master"
 # fi
 
-echo "Diffing with" "$$TRAVIS_COMMIT_RANGE"
+echo "Diffing with" "$TRAVIS_COMMIT_RANGE"
 
-for file in $(git diff --name-only "$$TRAVIS_COMMIT_RANGE" | grep .sh\$); do
+for file in $(git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep .sh\$); do
   # Globally ignore lint error SC2024: https://github.com/koalaman/shellcheck/wiki/SC2024
   # as I don't think it's an important check for our use case
   echo "Checking ${bold}$file${normal}..."
