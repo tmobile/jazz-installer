5bc274a0a74d60c6990fb76d9f878292f4e61086
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-09-07 11:29:52 -0400

    Add logging to test
    Former-commit-id: f2ee2e8b6797be6deb70c381b2c33a265ca3d3af

--- test/check-bash.sh
+++ test/check-bash.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-if [[ -z "$TRAVIS_BRANCH" ]]; then
-  BRANCH="$TRAVIS_BRANCH"
+if [[ -z "$TRAVIS_PULL_REQUEST_BRANCH" ]]; then
+  BRANCH="$TRAVIS_PULL_REQUEST_BRANCH"
 else
   BRANCH="master"
 fi
 
+echo "Diffing with " $BRANCH "and TRAVIS_BRANCH is " "$TRAVIS_BRANCH"
+
 for file in $(git diff --name-only $BRANCH | grep .sh\$); do
-  shellcheck "$file"
+  # Globally ignore lint error SC2024: https://github.com/koalaman/shellcheck/wiki/SC2024
+  # as I don't think it's an important check for our use case
+  shellcheck -e SC2024 "$file"
 done
