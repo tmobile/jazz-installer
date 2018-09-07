7bcfd98e734da069e19ce5f4c68a3f4b9ee5b4cf
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-09-07 12:31:45 -0400

    Alright, this should work

--- test/check-bash.sh
+++ test/check-bash.sh
@@ -5,17 +5,11 @@ set -eo pipefail
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-# if [[ -z $TRAVIS_BRANCH ]]; then
-#   BRANCH="$TRAVIS_BRANCH"
-# else
-#   BRANCH="master"
-# fi
-
-echo "Diffing with" "$TRAVIS_COMMIT_RANGE"
+echo "Diffing commit range" "$TRAVIS_COMMIT_RANGE"
 
 for file in $(git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep .sh\$); do
   # Globally ignore lint error SC2024: https://github.com/koalaman/shellcheck/wiki/SC2024
   # as I don't think it's an important check for our use case
   echo "Checking ${bold}$file${normal}..."
-  shellcheck -e SC2024 $file
+  shellcheck -e SC2024 "$file"
 done
