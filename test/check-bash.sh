0d7d23ee07c4c665274984b1210880d6f3ae19da
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-09-07 12:19:36 -0400

    Uhhh...this works locally?
    Former-commit-id: 0dd69607740f24e29b23ed672f947f25a9b493a1

--- test/check-bash.sh
+++ test/check-bash.sh
@@ -5,15 +5,15 @@ set -eo pipefail
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-if [[ -z $TRAVIS_BRANCH ]]; then
-  BRANCH="$TRAVIS_BRANCH"
-else
-  BRANCH="master"
-fi
+# if [[ -z $TRAVIS_BRANCH ]]; then
+#   BRANCH="$TRAVIS_BRANCH"
+# else
+#   BRANCH="master"
+# fi
 
-echo "Diffing with" "$BRANCH" " and TRAVIS_BRANCH is" "$TRAVIS_BRANCH"
+echo "Diffing with" "$TRAVIS_BRANCH"
 
-for file in $(git diff --name-only $TRAVIS_BRANCH | grep .sh\$); do
+for file in $(git diff --name-only "$TRAVIS_BRANCH" | grep .sh\$); do
   # Globally ignore lint error SC2024: https://github.com/koalaman/shellcheck/wiki/SC2024
   # as I don't think it's an important check for our use case
   echo "Checking ${bold}$file${normal}..."
