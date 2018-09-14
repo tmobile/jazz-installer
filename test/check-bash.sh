6010038ea5a03b150d94b71ae8bb8450352f6a45
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-09-14 12:17:34 -0400

    Exclude deleted files from Travis checks

--- test/check-bash.sh
+++ test/check-bash.sh
@@ -7,7 +7,8 @@ normal=$(tput sgr0)
 
 echo "Diffing commit range" "$TRAVIS_COMMIT_RANGE"
 
-for file in $(git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep .sh\$); do
+# Get changed file names, excluding deleted files
+for file in $(git diff --diff-filter=d --name-only "$TRAVIS_COMMIT_RANGE" | grep .sh\$); do
   # Globally ignore lint error SC2024: https://github.com/koalaman/shellcheck/wiki/SC2024
   # as I don't think it's an important check for our use case
   echo "Checking ${bold}$file${normal}..."
--- test/check-py.sh
+++ test/check-py.sh
@@ -7,7 +7,8 @@ normal=$(tput sgr0)
 
 echo "Diffing commit range" "$TRAVIS_COMMIT_RANGE"
 
-for file in $(git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep .py\$); do
+# Get changed file names, excluding deleted files
+for file in $(git diff --diff-filter=d --name-only "$TRAVIS_COMMIT_RANGE" | grep .py\$); do
   echo "Checking ${bold}$file${normal}..."
   flake8 "$file"
 done
