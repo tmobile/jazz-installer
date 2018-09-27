cbcc13bf7533217eb59276f819a69a50db8c7f64
Mike Merrell <mike.merrell3@t-mobile.com>
2018-09-27 11:04:28 -0700

    Lint fixes
    Former-commit-id: ff3a1f68efd1e2427f7de1f5f78f2ba2560d3cd5

--- feature-extensions/apigee/apigee-installer/init_apigee_install.py
+++ feature-extensions/apigee/apigee-installer/init_apigee_install.py
@@ -1,5 +1,4 @@
 import base64
-import glob
 import json
 import mimetools
 import os
@@ -7,21 +6,24 @@ import re
 import urllib2
 from zipfile import ZipFile
 
+
 def print_banner(message):
     print ("**************************************************")
     print (message)
     print ("**************************************************")
 
+
 def get_basic_auth(username, password):
     up = '%s:%s' % (username, password)
     return "Basic %s" % base64.b64encode(up).decode()
+
 
 def create_kvm(secretKey, reg, lambdaARN, host, org, env, username, password):
     print_banner("Creating the KVM for Common-Jazz API Proxy .......")
     payload = {
         "encrypted": "true",
         "entry": [
-            { 
+            {
                 "name": "accessKey",
                 "value": secretKey
             },
@@ -50,6 +52,7 @@ def create_kvm(secretKey, reg, lambdaARN, host, org, env, username, password):
     else:
         print_banner("KVM creation FAILED for the Common-Jazz API Proxy")
 
+
 def get_current_deployed_version(host, org, env, flow, username, password):
     print("Getting the current deployed version of %s in %s/%s on %s" % (flow, org, env, host))
     url = "%s/v1/o/%s/sharedflows/%s/deployments" % (host, org, flow)
@@ -66,6 +69,7 @@ def get_current_deployed_version(host, org, env, flow, username, password):
     print("  Deployed version: %s" % deployedVersion)
     return deployedVersion
 
+
 def is_api_deployed(host, org, env, name, revision, username, password):
     url = "%s/v1/o/%s/e/%s/apis/%s/revisions/%s/deployments" % (host, org, env, name, revision)
     req = urllib2.Request(url)
@@ -73,6 +77,7 @@ def is_api_deployed(host, org, env, name, revision, username, password):
     req.add_header('Accept', 'application/json')
     res = urllib2.urlopen(req)
     return res.getcode() == 200
+
 
 def import_item(zFile, host, org, name, importType, username, password):
     url = "%s/v1/o/%s/%s?action=import&name=%s" % (host, org, importType, name)
@@ -84,17 +89,22 @@ def import_item(zFile, host, org, name, importType, username, password):
         'Authorization': get_basic_auth(username, password),
         'Content-Type': "multipart/form-data; boundary=%s" % boundary
     }
-    data = "--%s\r\nContent-Disposition: form-data; name=\"file\"; filename=\"%s\"\r\nContent-Type: application/x-zip-compressed\r\n\r\n%s\r\n--%s--" % (boundary, zFile, fileContent, boundary)
+    data = (
+        ("--%s\r\nContent-Disposition: form-data; name=\"file\"; filename=\"%s\"\r\n" +
+            "Content-Type: application/x-zip-compressed\r\n\r\n%s\r\n--%s--")
+        % (boundary, zFile, fileContent, boundary))
     req = urllib2.Request(url, data, headers)
     res = urllib2.urlopen(req)
     result = json.load(res)
     return result['revision']
 
+
 def import_api(zFile, host, org, api, username, password):
     print("Importing new api proxy %s in %s on %s" % (api, org, host))
-    apiRevision = import_item(zFile, host, org, api, 'apis', username, password)    
+    apiRevision = import_item(zFile, host, org, api, 'apis', username, password)
     print("  New api proxy revision imported: %s" % apiRevision)
     return apiRevision
+
 
 def import_bundle(zFile, host, org, flow, username, password):
     print("Importing new SharedFlowBundle %s in %s on %s" % (flow, org, host))
@@ -102,45 +112,49 @@ def import_bundle(zFile, host, org, flow, username, password):
     print("  New revision imported: %s" % bundleRevision)
     return bundleRevision
 
+
 def undeploy(host, org, env, flow, revision, username, password):
     print("Undeploying the current deployed version of %s in %s/%s on %s" % (flow, org, env, host))
     url = "%s/v1/o/%s/e/%s/sharedflows/%s/revisions/%s/deployments" % (host, org, env, flow, revision)
-    req = urllib2.Request(url, 
-                          headers = { 'Authorization': get_basic_auth(username, password) })
+    req = urllib2.Request(url, headers={'Authorization': get_basic_auth(username, password)})
     req.get_method = lambda: 'DELETE'
     res = urllib2.urlopen(req)
     print(res.read())
 
+
 def deploy(host, org, env, flow, revision, username, password):
     print("Deploying the revision %s of %s in %s/%s on %s" % (revision, flow, org, env, host))
     url = "%s/v1/o/%s/e/%s/sharedflows/%s/revisions/%s/deployments?override=true" % (host, org, env, flow, revision)
-    req = urllib2.Request(url, 
-                          headers = { 'Authorization': get_basic_auth(username, password) })
+    req = urllib2.Request(url, headers={'Authorization': get_basic_auth(username, password)})
     req.get_method = lambda: 'POST'
     res = urllib2.urlopen(req)
     print(res.read())
 
+
 def deploy_api(host, org, env, apiName, revision, username, password):
     print("Deploying the revision %s of %s in %s/%s on %s" % (revision, apiName, org, env, host))
-    url = "%s/v1/o/%s/apis/%s/revisions/%s/deployments?action=deploy&env=%s&override=true" % (host, org, apiName, revision, env)
-    req = urllib2.Request(url, 
-                          headers = { 'Authorization': get_basic_auth(username, password) })
+    url = (
+        "%s/v1/o/%s/apis/%s/revisions/%s/deployments?action=deploy&env=%s&override=true"
+        % (host, org, apiName, revision, env))
+    req = urllib2.Request(url, headers={'Authorization': get_basic_auth(username, password)})
     req.get_method = lambda: 'POST'
     res = urllib2.urlopen(req)
     print(res.read())
     return is_api_deployed(host, org, env, apiName, revision, username, password)
 
+
 def stamp_build(path, item, build):
     flowFile = os.path.join(path, 'sharedflowbundle', "%s.xml" % item)
     with open(flowFile, 'r') as inFile:
         content = inFile.read()
-    
+
     content = re.sub('(?<=\<Description\>).+(?=\</Description\>)', build, content)
 
     with open(flowFile, 'w') as outFile:
         outFile.write(content)
-    
+
     print(item + ' stamped with build: ' + build)
+
 
 def zip_bundle(path, name, build):
     zfPath = os.path.join(path, "%s-%s.zip" % (name, build))
@@ -152,6 +166,7 @@ def zip_bundle(path, name, build):
             zf.write(sourcePath, sourcePath[offset:len(sourcePath)])
     zf.close()
     return zfPath
+
 
 def deploy_shared_flows(host, org, env, build, username, password):
     print_banner("Deploying Sharedflows now ............")
@@ -168,6 +183,7 @@ def deploy_shared_flows(host, org, env, build, username, password):
             zfPath.unlink()
     print_banner("Sharedflows deployment Complete")
 
+
 def deploy_common(host, org, env, username, password):
     print_banner("Importing the Common-Jazz API Proxy ......")
     commonRevision = import_api('Common-Jazz.zip', host, org, 'Common-Jazz', username, password)
@@ -177,6 +193,7 @@ def deploy_common(host, org, env, username, password):
     else:
         print_banner("Common-Jazz NOT deployed successfully")
     return success
+
 
 def install(secretKey, reg, lambdaARN, host, org, env, build, username, password):
     create_kvm(secretKey, reg, lambdaARN, host, org, env, username, password)
--- feature-extensions/apigee/apigee-installer/init_apigee_install_test.py
+++ feature-extensions/apigee/apigee-installer/init_apigee_install_test.py
@@ -1,6 +1,7 @@
 import unittest
 import init_apigee_install
 
+
 class TestTags(unittest.TestCase):
 
     def test_get_basic_auth(self):
@@ -10,8 +11,9 @@ class TestTags(unittest.TestCase):
             expected = 'Basic dGVzdHVzZXI6dGVzdHBhc3N3b3Jk'
             actual = init_apigee_install.get_basic_auth(username, password)
             self.assertEqual(expected, actual)
-        except :
+        except():
             pass
+
 
 if __name__ == '__main__':
     unittest.main()
