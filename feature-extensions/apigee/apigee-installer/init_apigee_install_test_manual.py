ff3a1f68efd1e2427f7de1f5f78f2ba2560d3cd5
Mike Merrell <mike.merrell3@t-mobile.com>
2018-09-27 11:04:28 -0700

    Lint fixes

--- feature-extensions/apigee/apigee-installer/init_apigee_install_test_manual.py
+++ feature-extensions/apigee/apigee-installer/init_apigee_install_test_manual.py
@@ -7,6 +7,7 @@ from zipfile import ZipFile
 apigeeUser = 'xxxx'
 apigeePassword = 'xxxx'
 
+
 def test_get_basic_auth():
     username = 'testuser'
     password = 'testpassword'
@@ -15,17 +16,33 @@ def test_get_basic_auth():
     assert(actual == expected)
     print 'Passed'
 
+
 def test_get_current_deployed_version():
     expected = '1'
-    actual = init_apigee_install.get_current_deployed_version('https://api.enterprise.apigee.com', 'tmobilea', 'sb02', 'InitializeRequest', apigeeUser, apigeePassword)
+    actual = init_apigee_install.get_current_deployed_version(
+        'https://api.enterprise.apigee.com',
+        'tmobilea',
+        'sb02',
+        'InitializeRequest',
+        apigeeUser,
+        apigeePassword)
     assert(actual == expected)
     print 'Passed'
 
+
 def test_is_api_deployed():
     expected = True
-    actual = init_apigee_install.is_api_deployed('https://api.enterprise.apigee.com', 'tmobilea', 'sb02', 'InitializeRequest', '1', apigeeUser, apigeePassword)
+    actual = init_apigee_install.is_api_deployed(
+        'https://api.enterprise.apigee.com',
+        'tmobilea',
+        'sb02',
+        'InitializeRequest',
+        '1',
+        apigeeUser,
+        apigeePassword)
     assert(actual == expected)
     print 'Passed'
+
 
 def test_stamp_build():
     flow = 'DataEncryption'
@@ -37,20 +54,22 @@ def test_stamp_build():
     init_apigee_install.stamp_build(path, flow, expected)
     with open(targetFile, 'r') as inFile:
         content = inFile.read()
-    
+
     actual = re.search('(?<=\<Description\>).+(?=\</Description\>)', content).group(0)
     assert(actual == expected)
     print 'Passed'
+
 
 def test_zip_bundle():
     name = 'DataEncryption'
     path = "./test/%s" % name
     build = 'Build.1.2.3'
-    sFiles = [ os.path.join(dir, addFile)
+    sFiles = [
+        os.path.join(dir, addFile)
         for dir, dirList, fileList in os.walk(os.path.join(path, 'sharedflowbundle'))
-            for addFile in fileList ]
-    expected = [ sourcePath[len(path) + 1:len(sourcePath)].replace('\\', '/')
-        for sourcePath in sFiles ]
+        for addFile in fileList]
+    expected = [sourcePath[len(path) + 1:len(sourcePath)].replace('\\', '/')
+                for sourcePath in sFiles]
     created = init_apigee_install.zip_bundle(path, name, build)
     actualFiles = []
     with ZipFile(created, 'r') as actual:
@@ -61,11 +80,21 @@ def test_zip_bundle():
         assert(f in actualFiles)
     print 'Passed'
 
+
 def test_import_item():
     zFile = './test/DataEncryption/DataEncryption-Build.1.2.3.zip'
-    actual = init_apigee_install.import_item(zFile, 'https://api.enterprise.apigee.com', 'tmobilea', 'DataEncryption', 'sharedflows', apigeeUser, apigeePassword)
-    assert(actual == '8')
+    actual = init_apigee_install.import_item(
+        zFile,
+        'https://api.enterprise.apigee.com',
+        'tmobilea',
+        'DataEncryption',
+        'sharedflows',
+        apigeeUser,
+        apigeePassword)
+    print "actual %s" % actual
+    assert(actual == '16')
     print 'Passed'
+
 
 def test_create_kvm():
     secretKey = 'alskduf109341243'
@@ -73,18 +102,51 @@ def test_create_kvm():
     lambdaARN = '1231lkjlkj123'
     org = 'tmobilea'
     env = 'sb02'
-    init_apigee_install.create_kvm(secretKey, reg, lambdaARN, 'https://api.enterprise.apigee.com', org, env, apigeeUser, apigeePassword)
+    init_apigee_install.create_kvm(
+        secretKey,
+        reg,
+        lambdaARN,
+        'https://api.enterprise.apigee.com',
+        org,
+        env,
+        apigeeUser,
+        apigeePassword)
+
 
 def test_deploy():
-    init_apigee_install.deploy('https://api.enterprise.apigee.com', 'tmobilea', 'sb02', 'DataEncryption', '8', apigeeUser, apigeePassword)
+    init_apigee_install.deploy(
+        'https://api.enterprise.apigee.com',
+        'tmobilea',
+        'sb02',
+        'DataEncryption',
+        '8',
+        apigeeUser,
+        apigeePassword)
+
 
 def test_undeploy():
-    init_apigee_install.undeploy('https://api.enterprise.apigee.com', 'tmobilea', 'sb02', 'DataEncryption', '8', apigeeUser, apigeePassword)
+    init_apigee_install.undeploy(
+        'https://api.enterprise.apigee.com',
+        'tmobilea',
+        'sb02',
+        'DataEncryption',
+        '8',
+        apigeeUser,
+        apigeePassword)
+
 
 def test_deploy_api():
-    actual = init_apigee_install.deploy_api('https://api.enterprise.apigee.com', 'tmobilea', 'sb02', 'TestNumberManagement', '1', apigeeUser, apigeePassword)
+    actual = init_apigee_install.deploy_api(
+        'https://api.enterprise.apigee.com',
+        'tmobilea',
+        'sb02',
+        'TestNumberManagement',
+        '1',
+        apigeeUser,
+        apigeePassword)
     assert(actual)
     print 'Passed'
+
 
 # test_get_basic_auth()
 # test_get_current_deployed_version()
