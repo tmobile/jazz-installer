0808ab2033290e8dbd51cde308f40c5cf73b9e64
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-10-10 11:50:14 -0400

    Fix linting errors
    Former-commit-id: bedb7481a7fc6c156b3fc4ce26c50882c85b98a1

--- installscripts/jazz-terraform-unix-noinstances/scripts/configureApikey.sh
+++ installscripts/jazz-terraform-unix-noinstances/scripts/configureApikey.sh
@@ -5,9 +5,9 @@ PROD_ID=$3
 jenkinsjsonpropsfile=$4
 env_name_prefix=$5
 
-sed -i "s/{AWS_DEV_API_ID_DEFAULT}/$DEV_ID/g" $jenkinsjsonpropsfile
-sed -i "s/{AWS_STG_API_ID_DEFAULT}/$STG_ID/g" $jenkinsjsonpropsfile
-sed -i "s/{AWS_STG_API_ID_JAZZ}/$STG_ID/g" $jenkinsjsonpropsfile
-sed -i "s/{AWS_PROD_API_ID_JAZZ}/$PROD_ID/g" $jenkinsjsonpropsfile
-sed -i "s/{AWS_PROD_API_ID_DEFAULT}/$PROD_ID/g" $jenkinsjsonpropsfile
-sed -i "s/INSTANCE_PREFIX\".*.$/INSTANCE_PREFIX\": \"$env_name_prefix\",/g" $jenkinsjsonpropsfile
+sed -i "s/{AWS_DEV_API_ID_DEFAULT}/$DEV_ID/g" "$jenkinsjsonpropsfile"
+sed -i "s/{AWS_STG_API_ID_DEFAULT}/$STG_ID/g" "$jenkinsjsonpropsfile"
+sed -i "s/{AWS_STG_API_ID_JAZZ}/$STG_ID/g" "$jenkinsjsonpropsfile"
+sed -i "s/{AWS_PROD_API_ID_JAZZ}/$PROD_ID/g" "$jenkinsjsonpropsfile"
+sed -i "s/{AWS_PROD_API_ID_DEFAULT}/$PROD_ID/g" "$jenkinsjsonpropsfile"
+sed -i "s/INSTANCE_PREFIX\".*.$/INSTANCE_PREFIX\": \"$env_name_prefix\",/g" "$jenkinsjsonpropsfile"
