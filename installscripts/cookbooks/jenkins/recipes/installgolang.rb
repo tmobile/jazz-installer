d57cf184d6a1056ef15cc21928ab86805eccc196
Johnbush <johnbush20@gmail.com>
2018-12-21 13:35:30 +0530

    Path issue fixed chef recipe

--- installscripts/cookbooks/jenkins/recipes/installgolang.rb
+++ installscripts/cookbooks/jenkins/recipes/installgolang.rb
@@ -1,33 +1,40 @@
-# Installing golang 
-remote_file "#{Chef::Config['file_cache_path']}/go1.10.3.linux-amd64.tar.gz" do
-    source 'https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz'
-    mode '0755'
-    action :create
-end
-   
-   
-bash 'update_paths' do
-    code <<-EOH
-    tar -C /usr/local -xzf #{Chef::Config['file_cache_path']}/go1.10.3.linux-amd64.tar.gz
-    echo 'export GOROOT=/usr/local/go' | sudo tee -a /etc/profile
-    echo 'export GOBIN=$GOROOT/bin' | sudo tee -a /etc/profile
-    echo 'export GOPATH=$GOROOT/src' | sudo tee -a /etc/profile
-    echo 'export PATH=$PATH:$GOPATH:$GOBIN' | sudo tee -a /etc/profile
-    source /etc/profile
-    EOH
-end
-   
-# Installing godep (Dependency Management tool) 
-remote_file "#{Chef::Config['file_cache_path']}/install.sh" do
-    source 'https://raw.githubusercontent.com/golang/dep/master/install.sh'
-    mode '0755'
-    action :create
-end
-   
-execute 'install_godep' do
-    command "sh #{Chef::Config['file_cache_path']}/install.sh" 
-end
-   
+if node['dockerizedJenkins'] == false
+    # Installing go lang
+    remote_file "#{Chef::Config['file_cache_path']}/go1.10.3.linux-amd64.tar.gz" do
+        source 'https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz'
+        mode '0755'
+        action :create
+    end
 
+    execute 'mkdir' do
+        command 'mkdir -p /opt/go/{bin,src,pkg}'
+    end
 
+    execute 'install_go' do
+        command "tar -C /usr/local -xzf #{Chef::Config['file_cache_path']}/go1.10.3.linux-amd64.tar.gz"
+    end
+
+    # Installing godep (Dependency Management tool)
+    remote_file "#{Chef::Config['file_cache_path']}/install.sh" do
+        source 'https://raw.githubusercontent.com/golang/dep/master/install.sh'
+        mode '0755'
+        action :create
+    end
+
+    execute 'install_godep' do
+        environment ({
+            'GOPATH' => "/opt/go",
+            'PATH' => "#{ENV['PATH']}:"+"#{ENV['GOPATH']}"+"/bin:"+"/usr/local/go/bin/"
+        })
+        command "bash #{Chef::Config['file_cache_path']}/install.sh"
+    end
+    # link go
+    link '/bin/go' do
+    to '/usr/local/go/bin/go'
+    end
+    # link dep
+    link '/bin/dep' do
+    to '/opt/go/bin/dep'
+    end
+end
 
