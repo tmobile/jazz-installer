22dc1f128391758150fec9a9aee42fd9ffd64d26
rajeevr2715 <rajeev.chettiyar@ust-global.com>
2019-02-06 14:49:41 +0530

    Updated the credentials to AWS

--- feature-extensions/multiaccount/account_cred.sh
+++ feature-extensions/multiaccount/account_cred.sh
@@ -4,12 +4,15 @@ CREDENTIAL_ID=$2
 ACCESSKEY=$3
 SECRETKEY=$4
 
+
 cat <<EOF | $JENKINS_CLI_CMD create-credentials-by-xml system::system::jenkins "(global)"
-<com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl>
+<com.cloudbees.jenkins.plugins.awscredentials.AWSCredentialsImpl plugin="aws-credentials@1.21">
   <scope>GLOBAL</scope>
   <id>$CREDENTIAL_ID</id>
-  <description>user created on bitbucket</description>
-  <username>$ACCESSKEY</username>
-  <password>$SECRETKEY</password>
-</com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl>
+  <description>AWS Credentials</description>
+  <accessKey>$ACCESSKEY</accessKey>
+  <secretKey>$SECRETKEY</secretKey>
+  <iamRoleArn></iamRoleArn>
+  <iamMfaSerialNumber></iamMfaSerialNumber>
+</com.cloudbees.jenkins.plugins.awscredentials.AWSCredentialsImpl>
 EOF
