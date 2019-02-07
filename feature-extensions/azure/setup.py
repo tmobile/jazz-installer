ed1ccc558851dea2445b6b6a4d02070fd874c408
Gabe Hicks <gabe.hicks@gmail.com>
2019-02-06 17:58:08 -0800

    Added environment variable handlers for all arguments to simplify a scripted install.

--- feature-extensions/azure/setup.py
+++ feature-extensions/azure/setup.py
@@ -34,33 +34,43 @@ def main(**kwargs):
 
 @main.command()
 @click.option('--jazz-stackprefix',
+              envvar='JAZZ_STACKPREFIX',
               help='Specify the stackprefix of your existing Jazz installation (e.g. myjazz), your existing config will be imported',
               prompt=True)
-@click.option('--scm-repo', help='Specify the scm repo url', prompt=True)
-@click.option('--scm-username', help='Specify the scm username', prompt=True)
-@click.option('--scm-password', help='Specify the scm password', prompt=True)
-@click.option('--scm-pathext', help='Specify the scm repo path ext (Use "scm" for bitbucket)', default="")
-@click.option('--jenkins-host', help='Specify the host of the Jenkins install', prompt=True)
-@click.option('--jenkins-port', help='Specify the port of the Jenkins install', prompt=True)
-@click.option('--jenkins-user', help='Admin username for configuration changes', prompt=True)
-@click.option('--jenkins-api-token', help='Admin API token for configuration changes', prompt=True)
-@click.option('--azure-subscription-id', help='Specify the ID for the azure subscription to deploy functions into',
+@click.option('--scm-repo', envvar='SCM_REPO', help='Specify the scm repo url', prompt=True)
+@click.option('--scm-username', envvar='SCM_USERNAME', help='Specify the scm username', prompt=True)
+@click.option('--scm-password', envvar='SCM_PASSWORD', help='Specify the scm password', prompt=True)
+@click.option('--scm-pathext', envvar='SCM_PATHEXT', help='Specify the scm repo path ext (Use "scm" for bitbucket)',
+              default="")
+@click.option('--jenkins-host', envvar='JENKINS_HOST', help='Specify the host of the Jenkins install', prompt=True)
+@click.option('--jenkins-port', envvar='JENKINS_PORT', help='Specify the port of the Jenkins install', prompt=True)
+@click.option('--jenkins-user', envvar='JENKINS_USER', help='Admin username for configuration changes', prompt=True)
+@click.option('--jenkins-api-token', envvar='JENKINS_API_TOKEN', help='Admin API token for configuration changes',
               prompt=True)
-@click.option('--azure-location', help='Specify the location to install functions', prompt=True)
-@click.option('--azure-client-id', help='Specify the client id for the Service Principal used to build infrastructure',
+@click.option('--azure-subscription-id', envvar='AZURE_SUBSCRIPTION_ID',
+              help='Specify the ID for the azure subscription to deploy functions into',
               prompt=True)
-@click.option('--azure-client-secret', help='Specify the password for Service Principal', prompt=True)
-@click.option('--azure-tenant-id', help='Specify the Azure AD tenant id for the Service Principal', prompt=True)
-@click.option('--azure-company-name', help='Specify the company name used in the Azure API Management service',
+@click.option('--azure-location', envvar='AZURE_LOCATION', help='Specify the location to install functions',
               prompt=True)
-@click.option('--azure-company-email',
+@click.option('--azure-client-id', envvar='AZURE_CLIENT_ID',
+              help='Specify the client id for the Service Principal used to build infrastructure',
+              prompt=True)
+@click.option('--azure-client-secret', envvar='AZURE_CLIENT_SECRET', help='Specify the password for Service Principal',
+              prompt=True)
+@click.option('--azure-tenant-id', envvar='AZURE_TENANT_ID',
+              help='Specify the Azure AD tenant id for the Service Principal', prompt=True)
+@click.option('--azure-company-name', envvar='AZURE_COMPANY_NAME',
+              help='Specify the company name used in the Azure API Management service',
+              prompt=True)
+@click.option('--azure-company-email', envvar='AZURE_COMPANY_EMAIL',
               help='Specify the company contact email used in the Azure API Management service', prompt=True)
-@click.option('--azure-apim-dev-sku',
+@click.option('--azure-apim-dev-sku', envvar='AZURE_APIM_DEV_SKU',
               help='The SKU for the Azure API Management service for the development environment', default='Developer',
               show_default='Developer')
-@click.option('--azure-apim-stage-sku', help='The SKU for the Azure API Management service for the staging environment',
+@click.option('--azure-apim-stage-sku', envvar='AZURE_APIM_STAGE_SKU',
+              help='The SKU for the Azure API Management service for the staging environment',
               default='Developer', show_default='Developer')
-@click.option('--azure-apim-prod-sku',
+@click.option('--azure-apim-prod-sku', envvar='AZURE_APIM_PROD_SKU',
               help='The SKU for the Azure API Management service for the production environment', default='Developer',
               show_default='Developer')
 def install(jazz_stackprefix, scm_repo, scm_username, scm_password, scm_pathext, jenkins_host, jenkins_port,
