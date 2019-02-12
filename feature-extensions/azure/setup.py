7be6d2ce717196cd8f71af0040f0fc7074d8d603
Gabe Hicks <gabe.hicks@gmail.com>
2019-02-12 14:17:25 -0800

    Added envars to unisntall function.

--- feature-extensions/azure/setup.py
+++ feature-extensions/azure/setup.py
@@ -86,7 +86,7 @@ def install(jazz_stackprefix, scm_repo, scm_username, scm_password, scm_pathext,
         sys.exit(1)
 
     print(colors.OKGREEN +
-        "\nThis will install {0} functionality into your Jazz deployment.\n".format(featureName)
+          "\nThis will install {0} functionality into your Jazz deployment.\n".format(featureName)
           + colors.ENDC)
     print(
         colors.OKGREEN +
@@ -111,22 +111,30 @@ def install(jazz_stackprefix, scm_repo, scm_username, scm_password, scm_pathext,
 
 @main.command()
 @click.option('--jazz-stackprefix',
+              envvar='JAZZ_STACKPREFIX',
               help='Specify the stackprefix of your existing Jazz installation (e.g. myjazz), your existing config will be imported',
               prompt=True)
-@click.option('--scm-repo', help='Specify the scm repo url', prompt=True)
-@click.option('--scm-username', help='Specify the scm username', prompt=True)
-@click.option('--scm-password', help='Specify the scm password', prompt=True)
-@click.option('--scm-pathext', help='Specify the scm repo path ext (Use "scm" for bitbucket)', default='')
-@click.option('--azure-subscription-id', help='Specify the ID for the azure subscription to deploy functions into',
+@click.option('--scm-repo', envvar='SCM_REPO', help='Specify the scm repo url', prompt=True)
+@click.option('--scm-username', envvar='SCM_USERNAME', help='Specify the scm username', prompt=True)
+@click.option('--scm-password', envvar='SCM_PASSWORD', help='Specify the scm password', prompt=True)
+@click.option('--scm-pathext', envvar='SCM_PATHEXT', help='Specify the scm repo path ext (Use "scm" for bitbucket)',
+              default='')
+@click.option('--azure-subscription-id', envvar='AZURE_SUBSCRIPTION_ID',
+              help='Specify the ID for the azure subscription to deploy functions into',
               prompt=True)
-@click.option('--azure-location', help='Specify the location to install functions', prompt=True)
-@click.option('--azure-client-id', help='Specify the client id for the Service Principal used to build infrastructure',
+@click.option('--azure-location', envvar='AZURE_LOCATION', help='Specify the location to install functions',
               prompt=True)
-@click.option('--azure-client-secret', help='Specify the password for Service Principal', prompt=True)
-@click.option('--azure-tenant-id', help='Specify the Azure AD tenant id for the Service Principal', prompt=True)
-@click.option('--azure-company-name', help='Specify the company name used in the Azure API Management service',
+@click.option('--azure-client-id', envvar='AZURE_CLIENT_ID',
+              help='Specify the client id for the Service Principal used to build infrastructure',
               prompt=True)
-@click.option('--azure-company-email',
+@click.option('--azure-client-secret', envvar='AZURE_CLIENT_SECRET', help='Specify the password for Service Principal',
+              prompt=True)
+@click.option('--azure-tenant-id', envvar='AZURE_TENANT_ID',
+              help='Specify the Azure AD tenant id for the Service Principal', prompt=True)
+@click.option('--azure-company-name', envvar='AZURE_COMPANY_NAME',
+              help='Specify the company name used in the Azure API Management service',
+              prompt=True)
+@click.option('--azure-company-email', envvar='AZURE_COMPANY_EMAIL',
               help='Specify the company contact email used in the Azure API Management service', prompt=True)
 def uninstall(jazz_stackprefix, scm_repo, scm_username, scm_password, scm_pathext, azure_subscription_id,
               azure_location, azure_client_id, azure_client_secret, azure_tenant_id, azure_company_name,
