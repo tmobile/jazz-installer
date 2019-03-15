5e34a730d5b5b7ea39d4db1f3f7675c971872cff
Ben Leggett <benjamin.leggett1@t-mobile.com>
2019-03-15 19:31:34 -0400

    Actually fix all prints

--- installer/helpers/destroy/delete_event_source_mapping.py
+++ installer/helpers/destroy/delete_event_source_mapping.py
@@ -14,26 +14,26 @@ def delete_event_source_mapping(stackName):
     event_source_function = stackName + "-hndlr-dev"
 
     try:
-        print "Listing Event source mapping for function " + event_source_function
+        print("Listing Event source mapping for function " + event_source_function)
         list_event_output = subprocess.check_output([
             "aws", "lambda", "list-event-source-mappings", "--function-name",
             event_source_function
         ])
-        print list_event_output
+        print(list_event_output)
 
         # Parser the json payload
         json_parse = json.loads(list_event_output)
         if json_parse is not None and len(
                 json_parse['EventSourceMappings']) > 0:
             uuid = json_parse['EventSourceMappings'][0]['UUID']
-            print "uuid " + uuid
+            print("uuid " + uuid)
 
             # Deleting the Event source mapping
             delete_event_output = subprocess.check_output([
                 "aws", "lambda", "delete-event-source-mapping", "--uuid", uuid
             ])
-            print delete_event_output
+            print(delete_event_output)
         else:
             print("No Event source mapping found")
     except Exception as e:
-        print e
+        print(e)
