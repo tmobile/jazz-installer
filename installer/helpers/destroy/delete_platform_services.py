46e2fb87a9135ccfe7b9c341ba341274ed637efb
rajeevr2715 <rajeev.chettiyar@ust-global.com>
2019-10-04 11:47:45 +0530

    Boto3 rewrite in delete - cloudfront, eventsourcemapping and cloudformation

--- installer/helpers/destroy/delete_platform_services.py
+++ installer/helpers/destroy/delete_platform_services.py
@@ -1,68 +1,49 @@
-import json
-import subprocess
-import os
-import os.path
-
-
 def checkCFServiceAvailable(servicename):
-    return subprocess.call(
-        "aws cloudformation list-stack-resources --stack-name " + servicename + " --region " + region,
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT)
+    try:
+        return cform_client.list_stack_resources(StackName=servicename)
+    except Exception:
+        return False
 
 
 def deleteCFService(servicename):
-    return subprocess.call(
-        "aws cloudformation delete-stack --stack-name " + servicename + " --region " + region,
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT)
+    try:
+        return cform_client.delete_stack(StackName=servicename)
+    except Exception:
+        return False
 
 
 def getServicesList():
-    return subprocess.call(
-        'aws cloudformation list-stacks --region ' + region + ' --stack-status-filter \
-        "CREATE_IN_PROGRESS" "CREATE_FAILED" "CREATE_COMPLETE" "UPDATE_IN_PROGRESS" "UPDATE_COMPLETE" \
-        >> listservice.json',
-        shell=True)
+    paginator = cform_client.get_paginator('list_stacks')
+    return paginator.paginate(StackStatusFilter=["CREATE_IN_PROGRESS",
+                                                 "CREATE_FAILED",
+                                                 "CREATE_COMPLETE",
+                                                 "UPDATE_IN_PROGRESS",
+                                                 "UPDATE_COMPLETE"])
 
 
 def deleteCloudFormationService(service_name):
     return_code = checkCFServiceAvailable(service_name)
-    if return_code == 0:
+    if return_code is not False:
         print("Service::" + service_name +
               ": exists. Service deletion started.........")
         delete_return_code = deleteCFService(service_name)
-        if delete_return_code == 0:
+        if delete_return_code is not False:
             print("\tSuccessfully deleted service " + service_name)
         else:
-            print("\tError while deleting service " + service_name +
-                  " errorcode=" + delete_return_code)
+            print("\tError while deleting service " + service_name)
     else:
         print('Error service not found::' + service_name)
 
 
-def delete_platform_services(stackname, region_name, deleteClientServices=False):
-    global region
-    region = region_name
+def delete_platform_services(stackname, client, deleteClientServices=False):
+    global cform_client
+    cform_client = client
     print("\r\nStarting deletion of micro services\r\n\r\n")
 
     # Delete user services Cloud formations
-    fname = 'listservice.json'
-    if os.path.isfile(fname):
-        os.remove(fname)
-
     valresp = getServicesList()
-    if (valresp != 0):
-        exit(1)
-
-    jsonFile = open(fname, 'r')
-    values = json.load(jsonFile)
-    jsonFile.close()
-
-    ss = values['StackSummaries']
-
-    for item in ss:
-        if (item['StackName'].startswith(stackname)):
-            deleteCloudFormationService(item['StackName'])
+    for page in valresp:
+        stack = page['StackSummaries']
+        for output in stack:
+            if (output['StackName'].startswith(stackname)):
+                deleteCloudFormationService(output['StackName'])
--- installer/helpers/destroyprep.py
+++ installer/helpers/destroyprep.py
@@ -7,6 +7,9 @@ from installer.helpers.destroy.delete_cf_dists import delete_cf_dists
 def destroyprep(stackname, identity, region, all=False):
     # Delete the identity policy  - Created in terraform/scripts/ses.sh
     client = boto3.client('ses', region_name=region)
+    cloudfront_client = boto3.client('cloudfront', region_name=region)
+    lambda_client = boto3.client('lambda', region_name=region)
+    cloudformation_client = boto3.client('cloudformation', region_name=region)
     client.delete_identity_policy(
         Identity=identity,
         PolicyName="Policy-{0}".format(stackname)
@@ -15,9 +18,9 @@ def destroyprep(stackname, identity, region, all=False):
     print("Destroy all: {0} for stack {1}".format(all, stackname))
     # run python scripts to teardown
     if all:
-        delete_event_source_mapping(stackname, region)
-        delete_platform_services(stackname, region, True)
-        delete_cf_dists(stackname, region, True)
+        delete_event_source_mapping(stackname, lambda_client)
+        delete_platform_services(stackname, cloudformation_client, True)
+        delete_cf_dists(stackname, cloudfront_client, True)
     else:
-        delete_event_source_mapping(stackname, region)
-        delete_platform_services(stackname, region, False)
+        delete_event_source_mapping(stackname, lambda_client)
+        delete_platform_services(stackname, cloudformation_client, False)
