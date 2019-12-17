6895c2afad4b4aeb1ce9652f83ce64e99f69c18a
sapessi <stefano.buliani@gmail.com>
2019-12-17 19:25:09 +0000

    Fixed indentation in new config code

--- installer/terraform/scripts/config.py
+++ installer/terraform/scripts/config.py
@@ -27,7 +27,7 @@ accountname = envprefix
 # if we have account aliases configured then we'll use the first alias as the account name
 account_aliases = boto3.client('iam').list_account_aliases()['AccountAliases']
 if len(account_aliases) > 0:
-  accountname = account_aliases[0]
+    accountname = account_aliases[0]
 
 dynamodb = boto3.resource('dynamodb', region_name=region)
 table = dynamodb.Table(tablename)
